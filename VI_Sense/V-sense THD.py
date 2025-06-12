a04a414fc20605119d7aa4a5970d01e0fbfa0782
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-12 15:46:01 +0200

    Update test script VI_Sense/V-sense THD.py: -I2C_WRITE(device_address="0x68", field_info={'fieldname': 'internal_sin_gain', 'length': 4, 'registers': [{'REG': '0x1F', 'POS': 4, 'RegisterName': 'Internal sin register 1', 'RegisterLength': 8, 'Name': 'internal_sin_gain[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x0A))  # -60dB sinus amplitude | +I2C_WRITE(device_address="0x68", field_info={'fieldname': 'internal_sin_gain', 'length': 4, 'registers': [{'REG': '0x1F', 'POS': 4, 'RegisterName': 'Internal sin register 1', 'RegisterLength': 8, 'Name': 'internal_sin_gain[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x00))  # 0dB sinus amplitude | -  Desired value is 85dB | +  Desired value is 83dB | -# realize FFT expexted value = 85dB, error: +/- 5dB

--- VI_Sense/V-sense THD.py
+++ VI_Sense/V-sense THD.py
@@ -7,14 +7,29 @@ print(f'............ {Test_Name} ........')
 from Procedures import Playback
 from Procedures import Internal_sin_1kHz_1W # set the internal sinusoidal generator at 1kHz 1W
 I2C_WRITE(device_address="0x68", field_info={'fieldname': 'i2c_page_sel', 'length': 2, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x01))  #Page 1 in regmap
-I2C_WRITE(device_address="0x68", field_info={'fieldname': 'internal_sin_gain', 'length': 4, 'registers': [{'REG': '0x1F', 'POS': 4, 'RegisterName': 'Internal sin register 1', 'RegisterLength': 8, 'Name': 'internal_sin_gain[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x0A))  # -60dB sinus amplitude
+I2C_WRITE(device_address="0x68", field_info={'fieldname': 'internal_sin_gain', 'length': 4, 'registers': [{'REG': '0x1F', 'POS': 4, 'RegisterName': 'Internal sin register 1', 'RegisterLength': 8, 'Name': 'internal_sin_gain[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x00))  # 0dB sinus amplitude
 I2C_WRITE(device_address="0x68", field_info={'fieldname': 'i2c_page_sel', 'length': 2, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x01))  #Page 0 in regmap
 from Procedures import VI_SNS_turn_on
 '''
   Wait for 1ms, waiting for settling
   Measure from TDM V-sns digital output
-  Desired value is 85dB
+  Desired value is 83dB
  '''
 sleep(0.001)  # 1 ms
 
-# realize FFT expexted value = 85dB, error: +/- 5dB
+
+# realize FFT expexted value = 83dB, error: +/- 3dB
+fundamental_freq=1000
+harmonic_freq=3000 #3rd harmonic
+expected_vals = {'fundamental_freq': 60.0, 'harmonic_freq': 180.0}
+    error_spreads = {'fundamental_freq': 0.5, 'harmonic_freq': 1.0}
+FFT(
+            signal='CLK',
+            reference='"GND"',
+            signal_type='Analog',
+            sample_number=1000,
+            sample_time=0.001,
+            window='Hanning',
+            expected_values=expected_vals,
+            error_spreads=error_spreads
+        ) 
