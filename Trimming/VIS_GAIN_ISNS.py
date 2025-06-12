e4179f030cff377ee8ea085a6ba1edc2a9c8b02d
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-12 15:24:31 +0200

    Update test script Trimming/VIS_GAIN_ISNS.py: -print(f' ........ Source 100mA from SPKRP/SPKRM ........ ') | -source_current = 100e-3 | +print(f' ........ Source 500mA from SPKRP/SPKRM ........ ') | +source_current = 500e-3 | +from Procedures import CLASSD_OUT_CURR_TRIMM

--- Trimming/VIS_GAIN_ISNS.py
+++ Trimming/VIS_GAIN_ISNS.py
@@ -17,16 +17,19 @@ from time import sleep
 from Procedures import Startup
 from Procedures import Global_enable
 from Procedures import VI_SNS_turn_on
-print(f' ........ Source 100mA from SPKRP/SPKRM ........ ')
-source_current = 100e-3
+print(f' ........ Source 500mA from SPKRP/SPKRM ........ ')
+source_current = 500e-3
+from Procedures import CLASSD_OUT_CURR_TRIMM
 AFORCE(signal="OUTP",reference="GND",value=source_current, error_spread=source_current*0.01) # 1% error
+AFORCE(signal=OUTM,reference="GND",value=-source_current, error_spread=source_current*0.01) # 1% error
 sleep(0.1)
 isns_gain_source_measured = I2C_READ("0x68", field_info={'fieldname': 'i_sense', 'length': 16, 'registers': [{'REG': '0x6B', 'POS': 0, 'RegisterName': 'I SENSE readback reg 1', 'RegisterLength': 8, 'Name': 'i_sense[15:8]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 15, 'FieldLSB': 8, 'Attribute': 'RRRRRRRR', 'Default': '00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x6C', 'POS': 0, 'RegisterName': 'I SENSE readback reg 2', 'RegisterLength': 8, 'Name': 'i_sense[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]}, expected_value=0xFE)
-print(f' ........ Sink 100mA to SPKRP/SPKRM ........ ')
-AFORCE(signal="OUTP",reference="GND",value=-source_current, error_spread=-source_current*0.01) # 1% error
+print(f' ........ Sink 500mA to SPKRP/SPKRM ........ ')
+AFORCE(signal="OUTP",reference="GND",value=-source_current, error_spread=source_current*0.01) # 1% error
+AFORCE(signal=OUTM,reference="GND",value=source_current, error_spread=source_current*0.01) # 1% error
 sleep(0.1)
 isns_gain_sink_measured = I2C_READ("0x68", field_info={'fieldname': 'i_sense', 'length': 16, 'registers': [{'REG': '0x6B', 'POS': 0, 'RegisterName': 'I SENSE readback reg 1', 'RegisterLength': 8, 'Name': 'i_sense[15:8]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 15, 'FieldLSB': 8, 'Attribute': 'RRRRRRRR', 'Default': '00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x6C', 'POS': 0, 'RegisterName': 'I SENSE readback reg 2', 'RegisterLength': 8, 'Name': 'i_sense[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]}, expected_value=0x01)
-isns_gain_calculated = 0.2/(int(isns_gain_source_measured)-int(isns_gain_sink_measured))
+isns_gain_calculated = 2*0.5/(int(isns_gain_source_measured)-int(isns_gain_sink_measured))
 print(f'{Testname} value : {isns_gain_calculated} ')
 I2C_WRITE("0x68", field_info={'fieldname': 'otp_isense_gain', 'length': 14, 'registers': [{'REG': '0xBB', 'POS': 0, 'RegisterName': 'OTP FIELDS 11', 'RegisterLength': 8, 'Name': 'otp_isense_gain[13:8]', 'Mask': '0x3F', 'Length': 6, 'FieldMSB': 13, 'FieldLSB': 8, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}, {'REG': '0xBC', 'POS': 0, 'RegisterName': 'OTP FIELDS 12- TRACEABILITY 0', 'RegisterLength': 8, 'Name': 'otp_isense_gain[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(round(isns_gain_calculated)) )
 
