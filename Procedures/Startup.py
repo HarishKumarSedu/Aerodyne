df907881eefb3a18bd46cfc26be507ed1bb9175f
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-16 15:16:43 +0200

    Update test script Procedures/Startup.py: - | - | - | - | -

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -1,14 +1,14 @@
-Test_Name = 'Startup'
-print(f'............ {Test_Name} ........')
-from dfttools import *
-
-VFORCE(signal="VDD",reference="GND",value=1.8)
-VFORCE(signal="RESETB",reference="GND",value=1.8)
-VFORCE(signal="PVDD",reference="GND",value=3.7)
-
-
-
-
-
-
-
+Test_Name = 'Startup'
+print(f'............ {Test_Name} ........')
+from dfttools import *
+
+VFORCE(signal="VDD",reference="GND",value=1.8)
+VFORCE(signal="RESETB",reference="GND",value=1.8)
+VFORCE(signal="PVDD",reference="GND",value=3.7)
+
+
+
+
+
+
+
