5f2e7bb9f1f95757d328a23ba360a7f371efe644
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-16 14:32:01 +0200

    Update test script Reference/BG_Current.py: - | - | - | - | -

--- Reference/BG_Current.py
+++ Reference/BG_Current.py
@@ -1,27 +1,29 @@
-from dfttools import *
-from time import sleep
-import random
-
-Test_Name = 'BG_Current'
-from Procedures import Startup
-print(f'............ {Test_Name} ........')
-
-'''
------------------------------------------------
-  1.BG 1uA
-  2.Turn ON the part in active mode, temp=27°C, vddp=3.7V, vddd=1.2V, UVLO trimmed, enable ref_bg;
-  3.Wait 400 us;
-  4.Bring out to the analog test point (rfu_ds_ref_test_en_vddd=8d) the 0.4V reference output voltage. Target voltage is 0.4V.
-'''
-# select the page 
-# I2C_WRITE(device_address="0x68",field_info={'fieldname': 'i2c_page_sel', 'length': 2, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=)
-# 1. Enable ref_bg  select the field 
-# I2C_WRITE(device_address="0x68",field_info=,write_value=)
-# Bring out to the analog test point (rfu_ds_ref_test_en_vddd=8d) on "IODATA1"
-# I2C_WRITE(device_address="0x68",field_info=,write_value=)
-target_value = 1e-6 # 1uA
-error_spread = target_value*0.05 # 5% of target value
-measured_value = AMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=error_spread)
-error = abs(measured_value - target_value)/abs(target_value) *100
-print(f"Optimal measured value : {measured_value/ 1e-6}uA, Target vlaue : {target_value/ 1e-6}uA")
+from dfttools import *
+from time import sleep
+import random
+
+Test_Name = 'BG_Current'
+from Procedures import Startup
+from Procedures import Global_enable
+print(f'............ {Test_Name} ........')
+
+'''
+-----------------------------------------------
+  1.BG 0.8uA
+  2.Turn ON the part in active mode, temp=27Â°C, vddp=3.7V, vddd=1.2V, UVLO trimmed, enable ref_bg;
+  3.Wait 400 us;
+  4.Bring out to the analog test point (rfu_ds_ref_test_en_vddd=8d) the 1uA reference output current. Target current is 0.8uA.
+'''
+# Enabling test page
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
+# Enabling the analog TMUX
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x8)
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+
+target_value = 0.8e-6 # 0.8uA
+error_spread = target_value*0.15 # 15% of target value
+measured_value = AMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=error_spread)
+error = abs(measured_value - target_value)/abs(target_value) *100
+print(f"Optimal measured value : {measured_value}uA, Target value : {target_value}uA")
 print(f"Minimum Error: {error}%")
