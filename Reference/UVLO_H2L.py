f50b7877c407ff7cf5747d898a8c4b96db11b5b3
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-16 14:32:16 +0200

    Update test script Reference/UVLO_H2L.py: - | - | - | - | -

--- Reference/UVLO_H2L.py
+++ Reference/UVLO_H2L.py
@@ -1,45 +1,52 @@
-from dfttools import *
-from time import sleep
-import random
-from Procedures import Startup
-
-Test_Name = 'UVLO_H2L'
-print(f'............ {Test_Name} ........')
-
-'''
-Enable the bandgap, bring UVLO comparator output to pin "IODATA0" 
-through digital test mux and 
-sweep "PVDD" from 3V to 1.5V and record the value 
-at which the compator toggles
----------------------------------------------
-  1.UVLO H2L
-  2.Turn ON the part in active mode, temp=27°C, vddd=1.2V, ref_bg enabled;
-  3.Wait 50 us;
-  4.Observe the ana_ds_pvdd_uvlo signal at the digital MUX and sweep vddp from 3.7V to 0V;
-  5.Store the vddp value for which the ana_ds_pvdd_uvlo goes from 0V to "VDD"; it is the H2L threshold of the UVLO.
-'''
-LH_Th = 2.25  # assumed 2.25V threshold expected trigger @,  designers must write 
-error_spread = 2.5e-3 # 2.5mv error spread
-force_voltage_low_limit = 0  # minimum voltage limit
-force_voltage_high_limit = 3.7  # max voltage limit
-force_voltage = force_voltage_low_limit
-
-while True:
-  # Add noise simulation
-  uvlo_forced_voltage = VFORCE(signal="IODATA0", reference="GND", value=force_voltage,error_spread=error_spread)
-  # Check trigger condition
-  trigger = VTRIG_LH(signal="PVDD", reference="GND", threshold=LH_Th, expected_value=force_voltage)
-
-  if trigger:
-    break
-  elif uvlo_forced_voltage >= force_voltage_high_limit:
-    print(f'..... Voltage max limit {force_voltage_low_limit}V crossed ........')
-    break
-
-  force_voltage += 0.05  # increase voltage by 50mV
-  sleep(0.01)  # 10 ms delay
-  
-error = abs(uvlo_forced_voltage - LH_Th)/abs(LH_Th) * 100
-print(f"Optimal Measured(force sweeped) Value: {uvlo_forced_voltage:.4f} V (Target: {LH_Th} V)")
-print(f"Minimum Error: {error:.6f} %")
-
+from dfttools import *
+from time import sleep
+import random
+from Procedures import Startup
+from Procedures import Global_enable
+
+Test_Name = 'UVLO_H2L'
+print(f'............ {Test_Name} ........')
+
+'''
+Enable the bandgap, bring UVLO comparator output to pin "IODATA0" 
+through digital test mux and 
+sweep "PVDD" from 3V to 1.5V and record the value 
+at which the compator toggles
+---------------------------------------------
+  1.UVLO H2L
+  2.Turn ON the part in active mode, temp=27Â°C, vddd=1.2V, ref_bg enabled;
+  3.Wait 50 us;
+  4.Observe the ana_ds_pvdd_uvlo signal at the digital MUX and sweep vddp from 3.7V to 0V;
+  5.Store the vddp value for which the ana_ds_pvdd_uvlo goes from 0V to "VDD"; it is the H2L threshold of the UVLO.
+'''
+# Enabling test page
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
+#Enabling digital TMUX
+I2C_WRITE(device_address="0x38", field_info={'fieldname': 'dig_test_en', 'length': 7, 'registers': [{'REG': '0x03', 'POS': 0, 'RegisterName': 'DIGITAL_TEST_SETTINGS_1', 'RegisterLength': 8, 'Name': 'dig_test_en[6:0]', 'Mask': '0x7F', 'Length': 7, 'FieldMSB': 6, 'FieldLSB': 0, 'Attribute': '0NNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x1)
+I2C_WRITE(device_address="0x38", field_info={'fieldname': 'dig_test_sel', 'length': 7, 'registers': [{'REG': '0x04', 'POS': 0, 'RegisterName': 'DIGITAL_TEST_SETTINGS_2', 'RegisterLength': 8, 'Name': 'dig_test_sel[6:0]', 'Mask': '0x7F', 'Length': 7, 'FieldMSB': 6, 'FieldLSB': 0, 'Attribute': '0NNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x21)
+
+HL_Th = 1.96  # assumed 1.96V threshold expected trigger
+error_spread = HL_Th*0.05 # 5% error spread
+force_voltage_low_limit = 0  # minimum voltage limit
+force_voltage_high_limit = 3.7  # max voltage limit
+force_voltage = force_voltage_high_limit
+
+while True:
+  # Add noise simulation
+  pvdd_forced_voltage = VFORCE(signal="PVDD", reference="GND", value=force_voltage,error_spread=error_spread)
+  # Check trigger condition
+  trigger = VTRIG_HL(signal="IODATA0", reference="GND", threshold=HL_Th, expected_value=force_voltage)
+
+  if trigger:
+    break
+  elif pvdd_forced_voltage <= force_voltage_low_limit:
+    print(f'..... Voltage low limit {force_voltage_low_limit}V crossed ........')
+    break
+
+  force_voltage -= 0.05  # increase voltage by 50mV
+  sleep(0.001)  # 1 ms delay
+  
+error = abs(pvdd_forced_voltage - HL_Th)/abs(HL_Th) * 100
+print(f"Optimal Measured(force sweeped) Value: {pvdd_forced_voltage:.4f} V (Target: {HL_Th} V)")
+print(f"Minimum Error: {error:.6f} %")
+
