3d263623736d2603b98d7c83eda7ef9e5885e19e
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-16 14:28:52 +0200

    Update test script GPADC/GPADC_Vbat_4P8_Test.py: -     | -     | - | - | -

--- GPADC/GPADC_Vbat_4P8_Test.py
+++ GPADC/GPADC_Vbat_4P8_Test.py
@@ -13,8 +13,6 @@ Procedure
     3.Measure VBAT and save the result in Variable VB0
     4.Measure the Vbat register after 8 average save in the Variable CODE0
     5. Check that 890<CODE0<894
-    
-    
 
 '''
 I2C_WRITE(device_address="0x38",field_info={'fieldname': 'global_en', 'length': 1, 'registers': [{'REG': '0x0F', 'POS': 0, 'RegisterName': 'GLOBAL_EN_REG', 'RegisterLength': 8, 'Name': 'global_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
@@ -38,12 +36,6 @@ Vbat_max=expected_value+2
 
 HCODE0=I2C_READ(device_address="0x38",field_info={'fieldname': 'vbat_meas', 'length': 10, 'registers': [{'REG': '0x23', 'POS': 0, 'RegisterName': 'VBAT measurement reg 1', 'RegisterLength': 8, 'Name': 'vbat_meas[9:8]', 'Mask': '0x3', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': '000000RR', 'Default': '00', 'User': '00YYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x24', 'POS': 0, 'RegisterName': 'VBAT measurement reg 2', 'RegisterLength': 8, 'Name': 'vbat_meas[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},expected_value=0x37D)
 CODE0=int(HCODE0)
-
-
-
-
-
-
 
 if Vbat_min <= CODE0 <= Vbat_max:
   print(f'............ {Test_Name} Passed ........')
