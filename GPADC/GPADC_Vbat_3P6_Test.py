1016c89188463bb7b5f37085903cb8821843d82a
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-16 14:28:37 +0200

    Update test script GPADC/GPADC_Vbat_3P6_Test.py: -     | -     | -

--- GPADC/GPADC_Vbat_3P6_Test.py
+++ GPADC/GPADC_Vbat_3P6_Test.py
@@ -13,9 +13,6 @@ Procedure
     3.Measure VBAT and save the result in Variable VB0
     4.Measure the Vbat register after 8 average save in the Variable CODE0
     5. Check that 667<CODE0<671
-    
-    
-
 '''
 I2C_WRITE(device_address="0x38",field_info={'fieldname': 'global_en', 'length': 1, 'registers': [{'REG': '0x0F', 'POS': 0, 'RegisterName': 'GLOBAL_EN_REG', 'RegisterLength': 8, 'Name': 'global_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
 sleep(0.001)
