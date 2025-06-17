bf204e39e45aab99aab9e601cfdd1ccf2ef8869a
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-17 12:34:27 +0200

    Update test script ClassD/DC_VDDP_PSRR.py: -VFORCE(signal="PVDD", reference="GND", value=5, expected_value=5) # force 5v between "PVDD" wrt "GND" | +VFORCE(signal="PVDD", reference="GND", value=5) # force 5v between "PVDD" wrt "GND" | -VFORCE(signal="PVDD", reference="GND", value=2.5, expected_value=2.5)  # force 2.5v between "PVDD" wrt "GND" | +VFORCE(signal="PVDD", reference="GND", value=2.5)  # force 2.5v between "PVDD" wrt "GND"

--- ClassD/DC_VDDP_PSRR.py
+++ ClassD/DC_VDDP_PSRR.py
@@ -25,9 +25,9 @@ expected_VDDP_2p5V_value = 0
 # Step 2
 sleep(0.0001) 
 #Set  VDDP=5V
-VFORCE(signal="PVDD", reference="GND", value=5, expected_value=5) # force 5v between "PVDD" wrt "GND"
+VFORCE(signal="PVDD", reference="GND", value=5) # force 5v between "PVDD" wrt "GND"
 Vout_V1 = VMEASURE(signal="OUTP", reference="OUTN", expected_value = expected_VDDP_5V_value, error_spread = error_percentage)
-VFORCE(signal="PVDD", reference="GND", value=2.5, expected_value=2.5)  # force 2.5v between "PVDD" wrt "GND"
+VFORCE(signal="PVDD", reference="GND", value=2.5)  # force 2.5v between "PVDD" wrt "GND"
 #set VDDP=2.5V
 Vout_V2 = VMEASURE(signal="OUTP", reference="OUTN",expected_value = expected_VDDP_2p5V_value, error_spread = error_percentage)
 # print(f'Vout_V1 {Vout_V1} Vout_V2 : {Vout_V2} , {20*log10(abs(Vout_V1-Vout_V2) / 2.5)}')
