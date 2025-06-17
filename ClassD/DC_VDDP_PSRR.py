667be40583ceb1ac8aac5f04429c13719610d3a4
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-17 12:35:40 +0200

    Update test script ClassD/DC_VDDP_PSRR.py: -error_percentage = 0.1e-3 # 0.4% | +error_percentage = 1e-3 # 0.4%

--- ClassD/DC_VDDP_PSRR.py
+++ ClassD/DC_VDDP_PSRR.py
@@ -19,7 +19,7 @@ Step 4. Calculate the DC PSRR as 20*log10((|VOUT_5V - VOUT_2p5V|)/2.5)
 # Step 1
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=1) 
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'force_dac_zero', 'length': 1, 'registers': [{'REG': '0x1E', 'POS': 1, 'RegisterName': 'Force registers 7', 'RegisterLength': 8, 'Name': 'force_dac_zero', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': '000NNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=1) 
-error_percentage = 0.1e-3 # 0.4%
+error_percentage = 1e-3 # 0.4%
 # Step 2
 sleep(0.0001) 
 #Set  VDDP=5V
