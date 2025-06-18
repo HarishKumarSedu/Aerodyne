b5c8e47f8baf0f55b7b57bebb1679de7775eaf4f
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-18 11:16:13 +0200

    Update test script VI_Sense/I-sense THD.py: -from Procedures import Internal_sin_1kHz_1W # set the internal sinusoidal generator at 1kHz 1W | +# from Procedures import Internal_sin_1kHz_1W # set the internal sinusoidal generator at 1kHz 1W

--- VI_Sense/I-sense THD.py
+++ VI_Sense/I-sense THD.py
@@ -5,7 +5,7 @@ Test_Name = 'V-sense THD'
 from Procedures import Startup
 print(f'............ {Test_Name} ........')
 from Procedures import Playback
-from Procedures import Internal_sin_1kHz_1W # set the internal sinusoidal generator at 1kHz 1W
+# from Procedures import Internal_sin_1kHz_1W # set the internal sinusoidal generator at 1kHz 1W
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=hex(0x01))  #Page 1 in regmap
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'internal_sin_gain', 'length': 4, 'registers': [{'REG': '0x1F', 'POS': 4, 'RegisterName': 'Internal sin register 1', 'RegisterLength': 8, 'Name': 'internal_sin_gain[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x00))  # 0dB sine amplitude
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'internal_sin_freq', 'length': 3, 'registers': [{'REG': '0x1F', 'POS': 0, 'RegisterName': 'Internal sin register 1', 'RegisterLength': 8, 'Name': 'internal_sin_freq[2:0]', 'Mask': '0x7', 'Length': 3, 'FieldMSB': 2, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(0x02))  # 1kHz sine frequncy
