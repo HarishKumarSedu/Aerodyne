ad805d655b7c9a702a96e05d34598081a717bef2
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-30 11:24:50 +0200

    Update test script Procedures/Startup.py: +from time import sleep | + | + | +sleep(0.001)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 Test_Name = 'Startup'
 print(f'............ {Test_Name} ........')
 from dfttools import *
@@ -5,6 +7,8 @@ from dfttools import *
 VFORCE(signal="VDD",reference="GND",value=1.8)
 VFORCE(signal="RESETB",reference="GND",value=1.8)
 VFORCE(signal="PVDD",reference="GND",value=3.7)
+
+sleep(0.001)
 
 # Switch to page 1
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0x1)
