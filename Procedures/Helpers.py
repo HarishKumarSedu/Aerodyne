d57c7b92444abf4c176aa4c930a28f472089a959
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-30 17:45:36 +0200

    Update test script Procedures/Helpers.py: - | -VMEASURE(signal="PVDD", reference = "GND", expected_value = 1.8 , error_spread = 1.7*0.1) | +v1 = VMEASURE(signal="PVDD", reference = "GND", expected_value = 1.8 , error_spread = 1.7*0.1) | +print(v1)

--- Procedures/Helpers.py
+++ Procedures/Helpers.py
@@ -1,2 +1,3 @@
 from dfttools import *
-VMEASURE(signal="PVDD", reference = "GND", expected_value = 1.8 , error_spread = 1.7*0.1)
+v1 = VMEASURE(signal="PVDD", reference = "GND", expected_value = 1.8 , error_spread = 1.7*0.1)
+print(v1)
