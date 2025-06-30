232594a99c27969a0b2b97ed76ae4df7567f00bd
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-30 12:02:07 +0200

    Update test script Digital/Memory Bist.py: +from dfttools import * | +from time import sleep | + | +Test_Name = 'MEMORY BIST' | +from Procedures import Startup

---
+++ Digital/Memory Bist.py
@@ -0,0 +1,34 @@
+from dfttools import *
+from time import sleep
+
+Test_Name = 'MEMORY BIST'
+from Procedures import Startup
+print(f'............ {Test_Name} ........')
+# designers must go through the details and correct the procedure
+'''
+Procedure 
+    1.Startup (it includes enable of test page)
+    2.Global_en to turn on the island
+    2.Start of the bist
+    3.Check the result
+'''
+
+# Set {'fieldname': 'global_en', 'length': 1, 'registers': [{'REG': '0x0F', 'POS': 0, 'RegisterName': 'GLOBAL_EN_REG', 'RegisterLength': 8, 'Name': 'global_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]} to 1
+I2C_WRITE(device_address="0x38", field_info={'fieldname': 'global_en', 'length': 1, 'registers': [{'REG': '0x0F', 'POS': 0, 'RegisterName': 'GLOBAL_EN_REG', 'RegisterLength': 8, 'Name': 'global_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0x1)
+
+# Switch to page 1
+I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 2, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x1)
+
+# Start of bist
+I2C_WRITE(device_address="0x38", field_info={'fieldname': 'bist_en', 'length': 1, 'registers': [{'REG': '0x28', 'POS': 0, 'RegisterName': 'BIST reg', 'RegisterLength': 8, 'Name': 'bist_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000RRNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x1)
+
+sleep (0.00002);
+
+# Read of the result
+BIST_RESULT_BIT=I2C_READ(device_address="0x38",field_info={'fieldname': 'bist_result', 'length': 1, 'registers': [{'REG': '0x28', 'POS': 2, 'RegisterName': 'BIST reg', 'RegisterLength': 8, 'Name': 'bist_result', 'Mask': '0x4', 'Length': 1, 'FieldMSB': 2, 'FieldLSB': 2, 'Attribute': '0000RRNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},expected_value=0x0)
+BIST_END_BIT=I2C_READ(device_address="0x38",field_info={'fieldname': 'bist_end', 'length': 1, 'registers': [{'REG': '0x28', 'POS': 3, 'RegisterName': 'BIST reg', 'RegisterLength': 8, 'Name': 'bist_end', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': '0000RRNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},expected_value=0x1)
+
+if ((BIST_END_BIT = 1) and(BIST_RESULT_BIT = 0)):
+  print(f'............ {Test_Name} Passed ........')
+
+
