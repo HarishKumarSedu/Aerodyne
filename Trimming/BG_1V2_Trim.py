4b3822426d3aefe4dd56490b18b276dd397eb334
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-07-02 15:13:36 +0200

    Update test script Trimming/BG_1V2_Trim.py: - | - | - | - | -

--- Trimming/BG_1V2_Trim.py
+++ Trimming/BG_1V2_Trim.py
@@ -1,72 +1,68 @@
-from dfttools import *
-from time import sleep
-import random
-
-Test_Name = 'BG_1V2_Trim'
-from Procedures import Startup
-print(f'............ {Test_Name} ........')
-'''
-enable ref_bg, # designer needs identify
-{'fieldname': 'otp_ds_ref_bg_ptat_trm', 'length': 3, 'registers': [{'REG': '0xB3', 'POS': 5, 'RegisterName': 'OTP FIELDS 3', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_ptat_trm[2:0]', 'Mask': '0xE0', 'Length': 3, 'FieldMSB': 2, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}<2:0>=0d, 
-{'fieldname': 'otp_ds_ref_bg_trm_0v6', 'length': 4, 'registers': [{'REG': '0xB0', 'POS': 0, 'RegisterName': 'OTP FIELDS 0', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_0v6[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}<3:0>=0d, 
-{'fieldname': 'otp_ds_ref_bg_trm_0v9', 'length': 4, 'registers': [{'REG': '0xB0', 'POS': 4, 'RegisterName': 'OTP FIELDS 0', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_0v9[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}<3:0>=0d, 
-{'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}<3:0>=0d; 
-'''
-I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_ptat_trm', 'length': 3, 'registers': [{'REG': '0xB3', 'POS': 5, 'RegisterName': 'OTP FIELDS 3', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_ptat_trm[2:0]', 'Mask': '0xE0', 'Length': 3, 'FieldMSB': 2, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
-I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_trm_0v6', 'length': 4, 'registers': [{'REG': '0xB0', 'POS': 0, 'RegisterName': 'OTP FIELDS 0', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_0v6[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
-I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_trm_0v9', 'length': 4, 'registers': [{'REG': '0xB0', 'POS': 4, 'RegisterName': 'OTP FIELDS 0', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_0v9[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
-I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
-# Bring out to the analog test point (rfu_ds_ref_test_en_vddd=4d) the 1.2V reference output voltage. Target voltage is 1.2V; 
-# I2C_WRITE(device_address="0x68",field_info=,write_value=)
-
-'''
-Bring 1.2V bandgap reference voltage to "IODATA1" through the analog test mux and trim it to the closest value to 1.2V
-'''
-
-# Initial value 
-percentage = 0.1 # 10% deviation
-
-typical_value = 1.2
-low_value = typical_value - typical_value*percentage
-high_value = typical_value + typical_value*percentage
-
-# Step size, designers needs to enter by lsb size
-step_size = 0.01 # 10mV
-
-# Number of steps width of the field / bits
-num_steps = 2**4  # 4-bit
-
-# Standard deviation for white noise
-noise_std_dev = 0.025
-
-# Initialize minimum error and optimal code
-min_error = float('inf')
-optimal_code = None
-optimal_measured_value = None
-
-for i in range(num_steps):
-    # sweep trimg code
-    I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=hex(i))
-    # Generate monotonic values with step size
-    expected_value = low_value + i * step_size 
-    # Add white noise to each value
-    measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=expected_value,error_spread=noise_std_dev)
-    error = abs(measured_value - typical_value)/abs(typical_value) *100
-    if error < min_error:
-        min_error = error
-        optimal_code = hex(i)
-        optimal_measured_value = measured_value
-    sleep(0.1)
-# Check for limits
-if low_value < optimal_measured_value < high_value:
-    print(f'............ {Test_Name} Passed ........')
-    # write the optimized code if the trim passed
-    I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=optimal_code)
-else:
-    print(f'............ {Test_Name} Failed ........')
-    # if the trimh failed program detult zero
-    I2C_WRITE(device_address="0x68",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0)
-print(f"Optimal Code: {optimal_code}")
-print(f"Optimal measured value : {optimal_measured_value}V, Target vlaue : {typical_value}V")
-
-print(f"Minimum Error: {min_error}%")
+from dfttools import *
+from time import sleep
+import random
+
+Test_Name = 'BG_1V2_Trim'
+from Procedures import Startup
+from Procedures import Global_enable
+print(f'............ {Test_Name} ........')
+
+# Enabling test page
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'i2c_page_sel', 'length': 2, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+# Enabling analog TMUX
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x4)
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en_buff', 'length': 1, 'registers': [{'REG': '0x10', 'POS': 6, 'RegisterName': 'FORCING_REG_2', 'RegisterLength': 8, 'Name': 'ref_test_en_buff', 'Mask': '0x40', 'Length': 1, 'FieldMSB': 6, 'FieldLSB': 6, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+'''
+Bring 1.2V bandgap reference voltage to "IODATA1" through the analog test mux and trim it to the closest value to 1.2V
+'''
+
+# Initial value 
+percentage = 1e-2 # 1% deviation
+
+typical_value = 1.2
+low_value = typical_value - typical_value*percentage
+high_value = typical_value + typical_value*percentage
+
+buffer_offset = 10e-3 # 10mV
+# Step size (LSB size)
+step_size = 7.57e-3 # 7.57mV
+
+# Number of steps width of the field / bits
+num_steps = 2^4  # 4-bit
+
+# Standard deviation for white noise
+noise_std_dev = 0.025
+
+# Initialize minimum error and optimal code
+min_error = float('inf')
+optimal_code = None
+optimal_measured_value = None
+
+for i in range(num_steps):
+    # sweep trimg code
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=hex(i))
+    # Generate monotonic values with step size
+    expected_value = low_value + i * step_size 
+    # Add white noise to each value and subtracting the buffer offset
+    measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=expected_value,error_spread=noise_std_dev) - buffer_offset
+    error = abs(measured_value - typical_value)/abs(typical_value) *100
+    if error < min_error:
+        min_error = error
+        optimal_code = hex(i)
+        optimal_measured_value = measured_value
+    sleep(0.001) # 1ms
+# Check for limits
+if low_value < optimal_measured_value < high_value:
+    print(f'............ {Test_Name} Passed ........')
+    # write the optimized code if the trim passed
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=optimal_code)
+else:
+    print(f'............ {Test_Name} Failed ........')
+    # if the trimh failed program default zero
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'otp_ds_ref_bg_trm_1v2', 'length': 4, 'registers': [{'REG': '0xB1', 'POS': 0, 'RegisterName': 'OTP FIELDS 1', 'RegisterLength': 8, 'Name': 'otp_ds_ref_bg_trm_1v2[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x80', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0)
+print(f"Optimal Code: {optimal_code}")
+print(f"Optimal measured value : {optimal_measured_value}V, Target value : {typical_value}V")
+
+print(f"Minimum Error: {min_error}%")
