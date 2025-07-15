952c11cb029c5bbdc217db919d456bb604cbbf4d
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-07-15 14:17:15 +0200

    Update test script ClassD/Cascode_P_Ron.py: -Test_Name = 'Low_Side_P_Ron' | +Test_Name = 'Cascode_P_Ron'

--- ClassD/Cascode_P_Ron.py
+++ ClassD/Cascode_P_Ron.py
@@ -2,7 +2,7 @@ from dfttools import *
 from time import sleep
 import random
 
-Test_Name = 'Low_Side_P_Ron'
+Test_Name = 'Cascode_P_Ron'
 from Procedures import speakerOff
 
 print(f'............ {Test_Name} ........')
