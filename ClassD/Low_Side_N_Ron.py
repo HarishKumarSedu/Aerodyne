631b26354b3060d9be1f62bf1286583e6b740e82
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-07-15 14:17:49 +0200

    Update test script ClassD/Low_Side_N_Ron.py: -Test_Name = 'Low_Side_P_Ron' | +Test_Name = 'Low_Side_N_Ron'

--- ClassD/Low_Side_N_Ron.py
+++ ClassD/Low_Side_N_Ron.py
@@ -2,7 +2,7 @@ from dfttools import *
 from time import sleep
 import random
 
-Test_Name = 'Low_Side_P_Ron'
+Test_Name = 'Low_Side_N_Ron'
 from Procedures import speakerOff
 
 print(f'............ {Test_Name} ........')
