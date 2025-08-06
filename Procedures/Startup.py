6127fec393dc2846924b401982585e08db28c550
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-06 12:01:09 +0200

    Update test script Procedures/Startup.py: -sleep(0.001) | +sleep(0.1)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -14,7 +14,7 @@ VFORCE(signal="VDD",reference="GND",value=1.8)
 VFORCE(signal="RESETB",reference="GND",value=1.8)
 VFORCE(signal="PVDD",reference="GND",value=3.7)
 
-sleep(0.001)
+sleep(0.1)
 
 # Switch to page 1
 I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1)
