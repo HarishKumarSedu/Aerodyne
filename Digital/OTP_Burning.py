075b406dbc9499945e98ab77b0cff313ff563071
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-06 09:49:56 +0200

    Update test script Digital/OTP_Burning.py: - | -

--- Digital/OTP_Burning.py
+++ Digital/OTP_Burning.py
@@ -21,10 +21,8 @@ I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'lengt
 
 # Enable clock for otp fsm
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'force_otp_clk_on', 'length': 1, 'registers': [{'REG': '0x10', 'POS': 1, 'RegisterName': 'FORCING_REG_2', 'RegisterLength': 8, 'Name': 'force_otp_clk_on', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x1)
-
 #################### I2C writings of OTP shadow registers #####################
 
 #################### I2C writings of OTP shadow registers #####################
-
 # Start of burn
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'otp_burn', 'length': 1, 'registers': [{'REG': '0xAE', 'POS': 1, 'RegisterName': 'OTP control reg 1', 'RegisterLength': 8, 'Name': 'otp_burn', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': 'R0000NNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x1)
