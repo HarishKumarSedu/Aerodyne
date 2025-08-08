c38006a45183c91917949fc2669b4cc71ca6a793
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-08 15:39:19 +0200

    Update test script Trimming/UVLO_L2H_Trim.py: -#Enabling "PVDD" UVLO

--- Trimming/UVLO_L2H_Trim.py
+++ Trimming/UVLO_L2H_Trim.py
@@ -20,7 +20,6 @@ target value is 2.25V; this leads to a reference voltage is 0.4V for the UVLO;
 '''
 #Enabling test page
 I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1
-#Enabling "PVDD" UVLO
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'debug_in_en', 'length': 7, 'registers': [{'REG': '0x22', 'POS': 0, 'RegisterName': 'Debug resgister 1', 'RegisterLength': 8, 'Name': 'debug_in_en[6:0]', 'Mask': '0x7F', 'Length': 7, 'FieldMSB': 6, 'FieldLSB': 0, 'Attribute': '0NNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x4)
 
 #Enabling digital TMUX
