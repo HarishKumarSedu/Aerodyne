264a0db081a88e5bba32b5fa155b59aff24f0940
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-09 14:02:00 +0200

    Update test script Procedures/Startup.py: -# VFORCE(signal="VDD",reference="GND",value=1.8) | -# VFORCE(signal="RESETB",reference="GND",value=1.8) | +VFORCE(signal="VDD",reference="GND",value=1.8) | +VFORCE(signal="RESETB",reference="GND",value=1.8)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -10,8 +10,8 @@ import sys
 Test_Name = 'Startup'
 print(f'............ {Test_Name} ........')
 from dfttools import *
-# VFORCE(signal="VDD",reference="GND",value=1.8)
-# VFORCE(signal="RESETB",reference="GND",value=1.8)
+VFORCE(signal="VDD",reference="GND",value=1.8)
+VFORCE(signal="RESETB",reference="GND",value=1.8)
 VFORCE(signal="PVDD",reference="GND",value=3.7)
 
 sleep(0.2)
