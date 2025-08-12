b18add2cd3dd888903f8d083dfc760a34de7ae9e
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-12 16:15:15 +0200

    Update test script Procedures/Startup.py: -Test_Name = 'Startup' | -print(f'............ {Test_Name} ........') | -VFORCE(signal="VDD",reference="GND",value=1.8) | -VFORCE(signal="RESETB",reference="GND",value=1.8) | -VFORCE(signal="PVDD",reference="GND",value=3.7)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -7,20 +7,21 @@ import sys
 # command = f"{sys.executable} -m pip install git+https://github.com/HarishKumarSedu/dfttools.git@main"
 # os.system(command)
 
-Test_Name = 'Startup'
-print(f'............ {Test_Name} ........')
 from dfttools import *
-VFORCE(signal="VDD",reference="GND",value=1.8)
-VFORCE(signal="RESETB",reference="GND",value=1.8)
-VFORCE(signal="PVDD",reference="GND",value=3.7)
+def startup():
+  Test_Name = 'Startup'
+  print(f'............ {Test_Name} ........')
+  VFORCE(signal="VDD",reference="GND",value=1.8)
+  VFORCE(signal="RESETB",reference="GND",value=1.8)
+  VFORCE(signal="PVDD",reference="GND",value=3.7)
 
-sleep(0.2)
+  sleep(0.2)
 
-# Switch to page 1
-I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1)
-# Unlock test page 
-I2C_REG_WRITE( device_address="0x38", register_address=0x2F, write_value=0xAA,PageNo=1)
-I2C_REG_WRITE( device_address="0x38", register_address=0x2F, write_value=0xBB,PageNo=1)
-# Switch to page 0
-I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x00,PageNo=0)
+  # Switch to page 1
+  I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1)
+  # Unlock test page 
+  I2C_REG_WRITE( device_address="0x38", register_address=0x2F, write_value=0xAA,PageNo=1)
+  I2C_REG_WRITE( device_address="0x38", register_address=0x2F, write_value=0xBB,PageNo=1)
+  # Switch to page 0
+  I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x00,PageNo=0)
 
