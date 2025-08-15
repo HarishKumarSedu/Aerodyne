1a784bcead3b49b8df29827360e0930df99ea1d9
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-15 14:21:28 +0200

    Update test script Procedures/CLASSD_OUT_POS.py: -print(f'............ {Test_Name} ........') | -def classD_out_pos(): | +def classd_out_pos(): | +  print(f'............ {Test_Name} ........') | +

--- Procedures/CLASSD_OUT_POS.py
+++ Procedures/CLASSD_OUT_POS.py
@@ -1,8 +1,8 @@
 Test_Name = 'CLASSD_OUT_POS'
-print(f'............ {Test_Name} ........')
 from dfttools import *
 from time import sleep
-def classD_out_pos():
+def classd_out_pos():
+  print(f'............ {Test_Name} ........')
   #I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0) # page 0
   I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1
 
@@ -31,3 +31,6 @@ def classD_out_pos():
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'cld_dvr_force_sel', 'length': 8, 'registers': [{'REG': '0x9C', 'POS': 0, 'RegisterName': 'CLD analog setting reg 6', 'RegisterLength': 8, 'Name': 'cld_dvr_force_sel[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0xA5)  # set bridge switch (LSp off, MIDp on, CASp off, HSp on, LSn on, MIDn off, CASn on, HSn off)
   # Wait for device stabilization
   sleep(0.0001)  # 100 µs
+
+if __name__ == '__main__':
+  classd_out_pos()
