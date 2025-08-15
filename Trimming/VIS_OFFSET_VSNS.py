f6173b49d0697144af5bd83e3c8a06435b8a7e54
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-15 14:48:04 +0200

    Update test script Trimming/VIS_OFFSET_VSNS.py: -   | +  vi_sns_turn_on()

--- Trimming/VIS_OFFSET_VSNS.py
+++ Trimming/VIS_OFFSET_VSNS.py
@@ -19,7 +19,7 @@ def vis_offset_vsns():
   startup()
   global_enable()
   classd_out_vmid()
-  
+  vi_sns_turn_on()
   sleep(0.1)
   vsns_offset_measured = I2C_READ("0x38", field_info={'fieldname': 'v_sense', 'length': 16, 'registers': [{'REG': '0x69', 'POS': 0, 'RegisterName': 'V SENSE readback reg 1', 'RegisterLength': 8, 'Name': 'v_sense[15:8]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 15, 'FieldLSB': 8, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x6A', 'POS': 0, 'RegisterName': 'V SENSE readback reg 2', 'RegisterLength': 8, 'Name': 'v_sense[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]}, expected_value=0x00)
   print(f'{Testname} value : {vsns_offset_measured} ')
