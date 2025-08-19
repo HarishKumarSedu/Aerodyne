7255d9fb2e3f2fa33fcaf9e7f8980a7c7b8850ee
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-19 17:12:45 +0200

    Update test script Procedures/VI_SNS_turn_on.py: -  global_enable()

--- Procedures/VI_SNS_turn_on.py
+++ Procedures/VI_SNS_turn_on.py
@@ -4,7 +4,6 @@ from time import sleep
 from Procedures.Global_enable import global_enable
 def vi_sns_turn_on():
   print(f'............ {Test_Name} ........')
-  global_enable()
   I2C_WRITE("0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0)
   I2C_WRITE("0x38", field_info={'fieldname': 'vis_adc_clk_pol_v', 'length': 1, 'registers': [{'REG': '0xA6', 'POS': 2, 'RegisterName': 'Vis modulator reg', 'RegisterLength': 8, 'Name': 'vis_adc_clk_pol_v', 'Mask': '0x4', 'Length': 1, 'FieldMSB': 2, 'FieldLSB': 2, 'Attribute': '0000NNNN', 'Default': '0x0C', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0)
   I2C_WRITE("0x38", field_info={'fieldname': 'vis_adc_clk_pol_i', 'length': 1, 'registers': [{'REG': '0xA6', 'POS': 3, 'RegisterName': 'Vis modulator reg', 'RegisterLength': 8, 'Name': 'vis_adc_clk_pol_i', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': '0000NNNN', 'Default': '0x0C', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0)
