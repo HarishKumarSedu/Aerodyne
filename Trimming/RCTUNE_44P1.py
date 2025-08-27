7768ac46a13d8204de7125fb4db364d8f3a05e66
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-27 10:42:21 +0200

    Update test script Trimming/RCTUNE_44P1.py: -from Procedures import Startup | -print(f'............ {Test_Name} ........') | -# designers must go through the details and correct the procedure | -''' | -Procedure

--- Trimming/RCTUNE_44P1.py
+++ Trimming/RCTUNE_44P1.py
@@ -2,28 +2,33 @@ from dfttools import *
 from time import sleep
 
 Test_Name = 'RCTUNE_44P1'
-from Procedures import Startup
-print(f'............ {Test_Name} ........')
-# designers must go through the details and correct the procedure
-'''
-Procedure 
-    before to start the bandgap has to be trimmed
-    1.Set 44.1KHz Family
-    2.Run rctune procedure
+from Procedures.Startup import startup
 
-'''
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'global_en', 'length': 1, 'registers': [{'REG': '0x0F', 'POS': 0, 'RegisterName': 'GLOBAL_EN_REG', 'RegisterLength': 8, 'Name': 'global_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
-sleep(0.001)
+def rctune_44p1():
+  startup()
+  print(f'............ {Test_Name} ........')
+  # designers must go through the details and correct the procedure
+  '''
+  Procedure 
+      before to start the bandgap has to be trimmed
+      1.Set 44.1KHz Family
+      2.Run rctune procedure
 
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'vis_rctun_sel', 'length': 1, 'registers': [{'REG': '0x60', 'POS': 3, 'RegisterName': 'VIS setting reg 0', 'RegisterLength': 8, 'Name': 'vis_rctun_sel', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': 'NNNNNNNN', 'Default': '0x0C', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x0)
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'pll_clk12m_bypass', 'length': 1, 'registers': [{'REG': '0x80', 'POS': 3, 'RegisterName': 'PLL_REG_1', 'RegisterLength': 8, 'Name': 'pll_clk12m_bypass', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': 'NNNNNNNN', 'Default': '0x21', 'User': '000YYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
+  '''
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'global_en', 'length': 1, 'registers': [{'REG': '0x0F', 'POS': 0, 'RegisterName': 'GLOBAL_EN_REG', 'RegisterLength': 8, 'Name': 'global_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '0000000Y', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
+  sleep(0.001)
 
-sleep(0.001)
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'rc_tun_start', 'length': 1, 'registers': [{'REG': '0x62', 'POS': 0, 'RegisterName': 'VIS setting reg 2', 'RegisterLength': 8, 'Name': 'rc_tun_start', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000P', 'Default': '0x00', 'User': '0000000Y', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
-sleep(0.001)
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'vis_rctun_sel', 'length': 1, 'registers': [{'REG': '0x60', 'POS': 3, 'RegisterName': 'VIS setting reg 0', 'RegisterLength': 8, 'Name': 'vis_rctun_sel', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': 'NNNNNNNN', 'Default': '0x0C', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x0)
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'pll_clk12m_bypass', 'length': 1, 'registers': [{'REG': '0x80', 'POS': 3, 'RegisterName': 'PLL_REG_1', 'RegisterLength': 8, 'Name': 'pll_clk12m_bypass', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': 'NNNNNNNN', 'Default': '0x21', 'User': '000YYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
+
+  sleep(0.001)
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'rc_tun_start', 'length': 1, 'registers': [{'REG': '0x62', 'POS': 0, 'RegisterName': 'VIS setting reg 2', 'RegisterLength': 8, 'Name': 'rc_tun_start', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000P', 'Default': '0x00', 'User': '0000000Y', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
+  sleep(0.001)
 
 
-print(f"Procedure finish")
+  print(f"Procedure finish")
 
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'vis_rctun_sel', 'length': 1, 'registers': [{'REG': '0x60', 'POS': 3, 'RegisterName': 'VIS setting reg 0', 'RegisterLength': 8, 'Name': 'vis_rctun_sel', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': 'NNNNNNNN', 'Default': '0x0C', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x0)
-sleep(0.001)
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'vis_rctun_sel', 'length': 1, 'registers': [{'REG': '0x60', 'POS': 3, 'RegisterName': 'VIS setting reg 0', 'RegisterLength': 8, 'Name': 'vis_rctun_sel', 'Mask': '0x8', 'Length': 1, 'FieldMSB': 3, 'FieldLSB': 3, 'Attribute': 'NNNNNNNN', 'Default': '0x0C', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x0)
+  sleep(0.001)
+if __name__ == '__main__':
+  rctune_44p1()
