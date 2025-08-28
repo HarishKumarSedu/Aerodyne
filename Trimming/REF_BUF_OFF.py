c046fbc8521ceeeed63dcc20d58b4a52de753940
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-08-28 11:46:11 +0200

    Update test script Trimming/REF_BUF_OFF.py: -def offset(): | +def offset(target=1.2): | -  force_value = 1.2 # forcing 1.2V on "ADDR" pin | +  force_value = target # forcing as default 1.2V on "ADDR" pin

--- Trimming/REF_BUF_OFF.py
+++ Trimming/REF_BUF_OFF.py
@@ -2,7 +2,7 @@ from dfttools import *
 from time import sleep
 import random
 
-def offset():
+def offset(target=1.2):
   Test_Name = 'REF_BUF_OFF'
   from Procedures import Startup
   from Procedures import Global_enable
@@ -17,7 +17,7 @@ def offset():
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_n_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 1, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_n_en', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
 
-  force_value = 1.2 # forcing 1.2V on "ADDR" pin
+  force_value = target # forcing as default 1.2V on "ADDR" pin
   error_spread = force_value*0.05
   forced_voltage=VFORCE(signal="ADDR", reference="GND", value=force_value, error_spread=error_spread)
   measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=force_value, error_spread=error_spread)
