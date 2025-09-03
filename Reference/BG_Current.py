e11aeebdfd6db8e14f0ae2807e1e3116e78cc243
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-09-03 11:43:11 +0200

    Update test script Reference/BG_Current.py: -from Procedures import Startup | -from Procedures import Global_enable | -print(f'............ {Test_Name} ........') | +from Procedures.Startup import startup | +from Procedures.Global_enable import global_enable

--- Reference/BG_Current.py
+++ Reference/BG_Current.py
@@ -3,27 +3,31 @@ from time import sleep
 import random
 
 Test_Name = 'BG_Current'
-from Procedures import Startup
-from Procedures import Global_enable
-print(f'............ {Test_Name} ........')
+from Procedures.Startup import startup
+from Procedures.Global_enable import global_enable
+def bg_current():
+  print(f'............ {Test_Name} ........')
 
-'''
+  '''
 -----------------------------------------------
   1.BG 0.8uA
   2.Turn ON the part in active mode, temp=27°C, vddp=3.7V, vddd=1.2V, UVLO trimmed, enable ref_bg;
   3.Wait 400 us;
   4.Bring out to the analog test point (rfu_ds_ref_test_en_vddd=8d) the 1uA reference output current. Target current is 0.8uA.
-'''
-# Enabling test page
-I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1
-# Enabling the analog TMUX
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x8)
-I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+  '''
+  # Enabling test page
+  I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1
+  # Enabling the analog TMUX
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x8)
+  I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
 
-target_value = 0.8e-6 # 0.8uA
-error_spread = target_value*0.15 # 15% of target value
-measured_value = AMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=error_spread)
-error = abs(measured_value - target_value)/abs(target_value) *100
-print(f"Optimal measured value : {measured_value}uA, Target value : {target_value}uA")
-print(f"Minimum Error: {error}%")
+  target_value = 0.8e-6 # 0.8uA
+  error_spread = target_value*0.15 # 15% of target value
+  measured_value = AMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=error_spread)
+  error = abs(measured_value - target_value)/abs(target_value) *100
+  print(f"Optimal measured value : {measured_value}uA, Target value : {target_value}uA")
+  print(f"Minimum Error: {error}%")
+
+if __name__ == '__main__':
+  bg_current()
