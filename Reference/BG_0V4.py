c5c85bbab1fc479d7e316a3d37a4d83a5f539a52
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-09-03 11:41:13 +0200

    Update test script Reference/BG_0V4.py: +from Trimming.REF_BUF_OFF import offset | +  target_value = 0.4 # 0.4V | +  buffer_offset = offset(target_value) # 10mV | -  target_value = 0.4 # 0.4V | -  buffer_offset = 10e-3 # 10mV

--- Reference/BG_0V4.py
+++ Reference/BG_0V4.py
@@ -5,10 +5,13 @@ import random
 Test_Name = 'BG_0v4'
 from Procedures.Startup import startup
 from Procedures.Global_enable import global_enable
+from Trimming.REF_BUF_OFF import offset
 def bg_0v4():
   print(f'............ {Test_Name} ........')
   startup()
   global_enable()
+  target_value = 0.4 # 0.4V
+  buffer_offset = offset(target_value) # 10mV
   # Enabling test page
   I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1
   # Enabling the analog TMUX
@@ -17,8 +20,6 @@ def bg_0v4():
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en_buff', 'length': 1, 'registers': [{'REG': '0x10', 'POS': 6, 'RegisterName': 'FORCING_REG_2', 'RegisterLength': 8, 'Name': 'ref_test_en_buff', 'Mask': '0x40', 'Length': 1, 'FieldMSB': 6, 'FieldLSB': 6, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
 
-  target_value = 0.4 # 0.4V
-  buffer_offset = 10e-3 # 10mV
   error_spread = target_value*0.04 # 4% of target value
   measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=error_spread) - buffer_offset
   error = abs(measured_value - target_value)/abs(target_value) *100
