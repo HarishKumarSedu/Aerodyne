a79f4a4d21aac9840220040d2a591ef13a49d6fb
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-09-03 11:14:02 +0200

    Update test script Trimming/OCP_LS_Trim.py: - | +  # if the trigger is already high for the highest code #750mA threshold end the test, chip can not be trimmed

--- Trimming/OCP_LS_Trim.py
+++ Trimming/OCP_LS_Trim.py
@@ -39,7 +39,7 @@ def ocp_ls_trim():
   trigger = VTRIG_LH(signal="IODATA0", reference="GND", threshold=1.8,expected_value=0) # digital signal trigger level is 1.8 
   AFORCE(signal="OUTP", reference="GND", value=i_force, error_spread=0.01)  # 500mA ±5%
   sleep(0.00005) #50us
-
+  # if the trigger is already high for the highest code #750mA threshold end the test, chip can not be trimmed 
   if trigger:
       print("Errore: all’avvio il comparatore è già alto")
   else:
