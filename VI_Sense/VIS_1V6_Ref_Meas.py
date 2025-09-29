b38856df32ae65e3551aa8d33b9895279b639e4c
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-09-29 20:21:16 +0200

    Update test script VI_Sense/VIS_1V6_Ref_Meas.py: -from Procedures import Startup | -from Procedures import VI_SNS_turn_on | +from Procedures.Startup import startup | +from Procedures.VI_SNS_turn_on import vi_sns_turn_on | -# Step 2: Settling Time

--- VI_Sense/VIS_1V6_Ref_Meas.py
+++ VI_Sense/VIS_1V6_Ref_Meas.py
@@ -12,24 +12,29 @@ VIS 1.5V Reference Measurement
 4. Measure 0V reference ground at "IODATA1" test point
 '''
 
-from Procedures import Startup
-from Procedures import VI_SNS_turn_on
+from Procedures.Startup import startup
+from Procedures.VI_SNS_turn_on import vi_sns_turn_on
 
-# Step 2: Settling Time
-sleep(0.0005)  # 500 µs
+def vis_1v6_ref_meas():
+  startup()
+  vi_sns_turn_on()
+  # Step 2: Settling Time
+  sleep(0.0005)  # 500 µs
 
-# Step 3: Route Reference to "ADDR" and "IODATA1"
-I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1  # Change page in regmap
-I2C_WRITE("0x38", field_info={'fieldname': 'vis_atp_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 1, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'vis_atp_en', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(1))  # Enable test mux
-I2C_WRITE("0x38", field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(5))  # Enable tests REF_1V6 and Reference "GND"
+  # Step 3: Route Reference to "ADDR" and "IODATA1"
+  I2C_REG_WRITE( device_address="0x38", register_address=0xFE, write_value=0x01,PageNo=1) # page 1  # Change page in regmap
+  I2C_WRITE("0x38", field_info={'fieldname': 'vis_atp_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 1, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'vis_atp_en', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(1))  # Enable test mux
+  I2C_WRITE("0x38", field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=hex(5))  # Enable tests REF_1V6 and Reference "GND"
 
-# Step 4: DC Voltage Measurement
-expected_dc1 = 1.6       # Target voltage on "ADDR"
-dc1_error = 0.005        # ±5mV tolerance
-vref1V5 = VMEASURE(signal="ADDR", reference="GND", expected_value=expected_dc1, error_spread=dc1_error)
-print(f'DC Reference Voltage: {vref1V5:.5f} V [Target: {expected_dc1:.2f}V ±{dc1_error*1000:.0f}mV]')
-# Pass/Fail Criteria
-if abs(vref1V5 - expected_dc1) <= dc1_error:
-    print("PASS: Voltage within ±5mV specification")
-else:
-    print(f"FAIL: Voltage error {abs(vref1V5-expected_dc1)*1000:.1f}mV exceeds limit")
+  # Step 4: DC Voltage Measurement
+  expected_dc1 = 1.6       # Target voltage on "ADDR"
+  dc1_error = 0.005        # ±5mV tolerance
+  vref1V5 = VMEASURE(signal="ADDR", reference="GND", expected_value=expected_dc1, error_spread=dc1_error)
+  print(f'DC Reference Voltage: {vref1V5:.5f} V [Target: {expected_dc1:.2f}V ±{dc1_error*1000:.0f}mV]')
+  # Pass/Fail Criteria
+  vref1V5 = VMEASURE(signal="ADDR", reference="GND", expected_value=float('Inf')) # open the path 
+  if abs(vref1V5 - expected_dc1) <= dc1_error:
+      print("PASS: Voltage within ±5mV specification")
+  else:
+      print(f"FAIL: Voltage error {abs(vref1V5-expected_dc1)*1000:.1f}mV exceeds limit")
+    
