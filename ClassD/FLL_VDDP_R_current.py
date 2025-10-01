a8e64bcba76a7470f71f05c2c3f56145b5e831ac
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-01 11:10:56 +0200

    Update test script ClassD/FLL_VDDP_R_current.py: -Test_Name = 'FLL_VDDP_R_current' | -from Procedures import Startup | -from Procedures import Playback | +from Procedures.Startup import startup | +from Procedures.Global_enable import global_enable

--- ClassD/FLL_VDDP_R_current.py
+++ ClassD/FLL_VDDP_R_current.py
@@ -1,26 +1,34 @@
 from dfttools import *
 from time import sleep 
 import random
-Test_Name = 'FLL_VDDP_R_current'
-from Procedures import Startup
-from Procedures import Playback
+from Procedures.Startup import startup
+from Procedures.Global_enable import global_enable
+from Procedures.Playback import playback
 
-print(f'............ {Test_Name} ........')
+def fll_vddp_r_current():
+  Test_Name = 'FLL_VDDP_R_current'
+  print(f'............ {Test_Name} ........')
+  startup()
+  global_enable()
+  playback()
 
-'''
-FLL_VDDP_R_current
-The FLL is trimmed, "IOCLK0" has 3.072MHz clock.
-Step 1. Bring out on "IODATA1" VDDP/R current of the FLL
-Step 2. Measure it.
-'''
+  '''
+  FLL_VDDP_R_current
+  The FLL is trimmed, "IOCLK0" has 3.072MHz clock.
+  Step 1. Bring out on "IODATA1" VDDP/R current of the FLL
+  Step 2. Measure it.
+  '''
 
-# Step 1
-I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=1)
-I2C_WRITE(device_address="0x38", field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=1) #enable "IODATA1"
-I2C_WRITE(device_address="0x38", field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=3) #bring out the VDDD/R current
-sleep(0.0001) 
-expected_value = 0.000001 #3.6/(3*1200000)->(vddp/3R)
-error_spread = expected_value*0.2
-measured_value=VMEASURE(signal="IODATA1", reference="GND", expected_value=expected_value, error_spread=error_spread)
+  # Step 1
+  I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=1)
+  I2C_WRITE(device_address="0x38", field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=1) #enable "IODATA1"
+  I2C_WRITE(device_address="0x38", field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=3) #bring out the VDDD/R current
+  sleep(0.0001) 
+  expected_value = 0.000001 #3.6/(3*1200000)->(vddp/3R)
+  error_spread = expected_value*0.2
+  measured_value=VMEASURE(signal="IODATA1", reference="GND", expected_value=expected_value, error_spread=error_spread)
 
-print(f'FLL VDDP/R current is: {measured_value}')
+  print(f'FLL VDDP/R current is: {measured_value}')
+
+if __name__ == '__main__':
+  fll_vddp_r_current()
