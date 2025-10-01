793851350539725d93d7c5cf6684d480c265b933
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-01 12:08:13 +0200

    Update test script ClassD/OCP_LS_TH.py: -  i_end = 0.85 | -  i_force = i_start | -      while i_force < i_end: | +      for code in range(15): | +

--- ClassD/OCP_LS_TH.py
+++ ClassD/OCP_LS_TH.py
@@ -27,8 +27,6 @@ def ocp_ls_trim():
   sleep(0.0001)  # 100 µs
   i_start = 0.5
   i_step = 0.05
-  i_end = 0.85
-  i_force = i_start
   AFORCE(signal="OUTP", reference="GND", value=i_force, error_spread=0.01)  # 500mA ±5%
   sleep(0.00005) #50us
   # mid_up = 0 #debug
@@ -37,16 +35,16 @@ def ocp_ls_trim():
   if ocp_status :
       print("Errore: all’avvio il comparatore è già alto")
   else:
-      while i_force < i_end:
+      for code in range(15):
+      
           AFORCE(signal="OUTP", reference="GND", value=i_start + i_step*code , error_spread=0.05) 
           sleep(50e-6)  # 50 µs
           print(f"current = {code*i_step + i_start :.3f} ")
           ocp_status =I2C_READ(device_address="0x38",field_info={'fieldname': 'cld_ocp_state', 'length': 1, 'registers': [{'REG': '0x05', 'POS': 1, 'RegisterName': 'Interrupt status register 2', 'RegisterLength': 8, 'Name': 'cld_ocp_state', 'Mask': '0x2', 'Length': 1, 'FieldMSB': 1, 'FieldLSB': 1, 'Attribute': 'IIIIIIII', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},expected_value=1)
         
-          if ocp_status :
-              print(f"Triggered @ = {i_force}A ")
+          if ocp_status:
+              print(f"Threshold = {code*i_step + i_start :.3f} ")
               break
-          i_force += i_step
           
   AFORCE(signal="OUTP", reference="PGND", value=float('inf'), error_spread=0.05)  # stop forcing
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'dig_test_en', 'length': 7, 'registers': [{'REG': '0x03', 'POS': 0, 'RegisterName': 'DIGITAL_TEST_SETTINGS_1', 'RegisterLength': 8, 'Name': 'dig_test_en[6:0]', 'Mask': '0x7F', 'Length': 7, 'FieldMSB': 6, 'FieldLSB': 0, 'Attribute': '0NNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x0) #only bit 2 need to be changed
