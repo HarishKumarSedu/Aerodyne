661abdcfabc8cbd2241b024a39ee4a6252a6982f
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-02 17:27:55 +0200

    Update test script ClassD/FLL_ramp_current.py: +  step 3 : this is a n2p current measure between "VDD" and "IODATA1" | -  measured_value=VMEASURE(signal="IODATA1", reference="GND", expected_value=expected_value, error_spread=error_spread) | +  measured_value=VMEASURE(signal="VDD", reference="IODATA1", expected_value=expected_value, error_spread=error_spread) | -  VMEASURE(signal="IODATA1", reference="GND", expected_value=float('Inf')) | +  VMEASURE(signal="VDD", reference="IODATA1", expected_value=float('Inf'))

--- ClassD/FLL_ramp_current.py
+++ ClassD/FLL_ramp_current.py
@@ -18,6 +18,7 @@ def fll_ramp_current():
   The FLL is trimmed, "IOCLK0" has 3.072MHz clock.
   Step 1. Bring out on "IODATA1" IRAMP VDDP/R-derived current of the FLL
   Step 2. Measure it.
+  step 3 : this is a n2p current measure between "VDD" and "IODATA1"
   '''
 
   # Step 1
@@ -28,10 +29,10 @@ def fll_ramp_current():
   sleep(0.0001) 
   expected_value = 0.0000015 #3.6/(3*1200000)*18/12->(vddp/2R)
   error_spread = expected_value*0.2
-  measured_value=VMEASURE(signal="IODATA1", reference="GND", expected_value=expected_value, error_spread=error_spread)
+  measured_value=VMEASURE(signal="VDD", reference="IODATA1", expected_value=expected_value, error_spread=error_spread)
 
   print(f'FLL ramp VDDP/R-derived current is: {measured_value}')
-  VMEASURE(signal="IODATA1", reference="GND", expected_value=float('Inf'))
+  VMEASURE(signal="VDD", reference="IODATA1", expected_value=float('Inf'))
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0) 
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'cld_pwm_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 5, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'cld_pwm_test_en', 'Mask': '0x20', 'Length': 1, 'FieldMSB': 5, 'FieldLSB': 5, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0)
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0) 
