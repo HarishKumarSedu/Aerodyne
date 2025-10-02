8fed021eb83d3d1352e20918a57665853ad8d12b
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-02 16:44:45 +0200

    Update test script Reference/BG_Ready.py: -  target_value = 1.2 # 1.2V | +  target_value = 1.8 # 1.8V

--- Reference/BG_Ready.py
+++ Reference/BG_Ready.py
@@ -21,7 +21,7 @@ Enable the bandgap, bring BG ready signal to pin "IOCLK1"
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'dig_test_en', 'length': 7, 'registers': [{'REG': '0x03', 'POS': 0, 'RegisterName': 'DIGITAL_TEST_SETTINGS_1', 'RegisterLength': 8, 'Name': 'dig_test_en[6:0]', 'Mask': '0x7F', 'Length': 7, 'FieldMSB': 6, 'FieldLSB': 0, 'Attribute': '0NNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x4)
   I2C_WRITE(device_address="0x38", field_info={'fieldname': 'dig_test_sel', 'length': 7, 'registers': [{'REG': '0x04', 'POS': 0, 'RegisterName': 'DIGITAL_TEST_SETTINGS_2', 'RegisterLength': 8, 'Name': 'dig_test_sel[6:0]', 'Mask': '0x7F', 'Length': 7, 'FieldMSB': 6, 'FieldLSB': 0, 'Attribute': '0NNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=0x21)
 
-  target_value = 1.2 # 1.2V
+  target_value = 1.8 # 1.8V
   error_spread = target_value*0.04 # 4% of target value
   measured_value = VMEASURE(signal="IOCLK1", reference="GND", expected_value=target_value,error_spread=error_spread)
   error = abs(measured_value - target_value)/abs(target_value) *100
