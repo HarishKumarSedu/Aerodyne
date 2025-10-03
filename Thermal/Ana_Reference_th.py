a4d0e865afad0c6206392289622c9fce4389413b
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-03 10:45:31 +0200

    Update test script Thermal/Ana_Reference_th.py: -      target_value = ref_test.get('error_percentage',0) # default is 0 | -      error_percentage = ref_test.get('target',0) # default is 0 | +      target_value = ref_test.get('target',0) # default is 0 | +      error_percentage = ref_test.get('error_percentage',0) # default is 0

--- Thermal/Ana_Reference_th.py
+++ Thermal/Ana_Reference_th.py
@@ -51,8 +51,8 @@ def ana_reference_th():
       I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en_buff', 'length': 1, 'registers': [{'REG': '0x10', 'POS': 6, 'RegisterName': 'FORCING_REG_2', 'RegisterLength': 8, 'Name': 'ref_test_en_buff', 'Mask': '0x40', 'Length': 1, 'FieldMSB': 6, 'FieldLSB': 6, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
       I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
       sleep(0.1)
-      target_value = ref_test.get('error_percentage',0) # default is 0
-      error_percentage = ref_test.get('target',0) # default is 0
+      target_value = ref_test.get('target',0) # default is 0
+      error_percentage = ref_test.get('error_percentage',0) # default is 0
       measured_value = AMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=target_value*error_percentage)
       I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
       AMEASURE(signal="IODATA1", reference="GND", expected_value=float('Inf')) # open switch 
