b3a9b2bce9fe515a05783763a917d2b5134ae07b
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-03 10:17:37 +0200

    Update test script Thermal/Ana_Reference_th.py: +    measured_value = 0 # default  | -      # update the measured value  | -      ref_test.update( | -        { | -          "measured_value" : measured_value

--- Thermal/Ana_Reference_th.py
+++ Thermal/Ana_Reference_th.py
@@ -30,6 +30,7 @@ def ana_reference_th():
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'atp_p_en', 'length': 1, 'registers': [{'REG': '0x17', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_EN2', 'RegisterLength': 8, 'Name': 'atp_p_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000NNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x1)
   
   for ref_test_name, ref_test in test_blk.items():
+    measured_value = 0 # default 
     # if the test is voltage measure select multimeter / if there is no unit default is ''
     if ref_test.get('unit','') == 'V':
       I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=ref_test.get('test_selection_code',1)) # test selection 
@@ -38,12 +39,7 @@ def ana_reference_th():
       target_value = ref_test.get('target',0) # default is 0
       measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=target_value*error_percentage)
       VMEASURE(signal="IODATA1", reference="GND", expected_value=float('Inf')) # open switch 
-      # update the measured value 
-      ref_test.update(
-        {
-          "measured_value" : measured_value
-        }
-      )
+
     # current measurements 
     if ref_test.get('unit','') == 'A':
       I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=ref_test.get('test_selection_code',1)) # test selection 
@@ -52,12 +48,12 @@ def ana_reference_th():
       target_value = ref_test.get('target',0) # default is 0
       measured_value = AMEASURE(signal="IODATA1", reference="GND", expected_value=target_value,error_spread=target_value*error_percentage)
       AMEASURE(signal="IODATA1", reference="GND", expected_value=float('Inf')) # open switch 
-      # update the measured value 
-      ref_test.update(
-        {
-          "measured_value" : measured_value
-        }
-      )
+    # update the measured value 
+    ref_test.update(
+      {
+        "measured_value" : measured_value
+      }
+    )
 
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
   I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
