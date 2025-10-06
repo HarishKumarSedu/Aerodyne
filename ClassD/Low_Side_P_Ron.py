99aaec22de51e2284077f2f56d0c749a7664a99b
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-06 09:44:31 +0200

    Update test script ClassD/Low_Side_P_Ron.py: -from Procedures.Ron_CLD_Setup import ron_cld_setup | +from Procedures.Ron_CLD import ron_cld_setup,ron_cld_unset | -# Ron_CLD_Unset() | +ron_cld_unset()

--- ClassD/Low_Side_P_Ron.py
+++ ClassD/Low_Side_P_Ron.py
@@ -3,7 +3,7 @@ from time import sleep
 import random
 
 Test_Name = 'Low_Side_P_Ron'
-from Procedures.Ron_CLD_Setup import ron_cld_setup
+from Procedures.Ron_CLD import ron_cld_setup,ron_cld_unset
 
 print(f'............ {Test_Name} ........')
 #I2C_WRITE(device_address="0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0) # page 0
@@ -26,6 +26,6 @@ print(f'[Expected ~30m Ohms typical, based on dV={dV:.3f}V @ {I_forced*1000:.0f}
 
 AFORCE(signal="OUTP", reference="PGND", value=float('inf'), error_spread=0.05)  # stop forcing
 
-# Ron_CLD_Unset()
+ron_cld_unset()
 
 I2C_WRITE(device_address="0x38", field_info={'fieldname': 'cld_dvr_force_sel', 'length': 8, 'registers': [{'REG': '0x9C', 'POS': 0, 'RegisterName': 'CLD analog setting reg 6', 'RegisterLength': 8, 'Name': 'cld_dvr_force_sel[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0x0) # select all switch off
