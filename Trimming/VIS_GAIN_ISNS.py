733530aae92d2866058b9519f289707138d26882
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-22 15:27:15 +0200

    Update test script Trimming/VIS_GAIN_ISNS.py: -  isns_current_sink_code = (0x10000 - isns_current_sink_code) if isns_current_sink_code & 0x8000 else isns_current_sink_code | +  isns_current_sink_code = (0x10000 + isns_current_sink_code) if isns_current_sink_code & 0x8000 else isns_current_sink_code

--- Trimming/VIS_GAIN_ISNS.py
+++ Trimming/VIS_GAIN_ISNS.py
@@ -45,7 +45,7 @@ def vis_gain_isns():
   sink_current_measured = AFORCE(signal="OUTP",reference="OUTN",value=-source_current, error_spread=source_current*0.01) # 1% error
   sleep(0.1)
   isns_current_sink_code = I2C_READ("0x38", field_info={'fieldname': 'i_sense', 'length': 16, 'registers': [{'REG': '0x6B', 'POS': 0, 'RegisterName': 'I SENSE readback reg 1', 'RegisterLength': 8, 'Name': 'i_sense[15:8]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 15, 'FieldLSB': 8, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x6C', 'POS': 0, 'RegisterName': 'I SENSE readback reg 2', 'RegisterLength': 8, 'Name': 'i_sense[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]}, expected_value=0xeaab)
-  isns_current_sink_code = (0x10000 - isns_current_sink_code) if isns_current_sink_code & 0x8000 else isns_current_sink_code
+  isns_current_sink_code = (0x10000 + isns_current_sink_code) if isns_current_sink_code & 0x8000 else isns_current_sink_code
   print(f' ........ Sink 500mA to SPKRP/SPKRM ........ Code {hex(isns_current_sink_code)} ')
   ######## Isense Gain Calculations #####################
   LSB = (20/2**15) # lsb of the 16 bit isense value 
