f41c585692686f32557d39107695af2432bfd8c3
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-27 11:00:10 +0100

    Update test script VI_Sense/VIS_1uA_Curr1_Meas.py: -    imeas = AMEASURE(signal="ADDR", reference="GND", expected_value=expected_curr, error_spread=curr_error) | +    imeas = AMEASURE(signal="IODATA1", reference="GND", expected_value=expected_curr, error_spread=curr_error)

--- VI_Sense/VIS_1uA_Curr1_Meas.py
+++ VI_Sense/VIS_1uA_Curr1_Meas.py
@@ -30,7 +30,7 @@ def vis_1ua_curr1_meas():
     # Step 4: Current Measurement
     expected_curr = 1.0e-6    # 1.0 uA target
     curr_error = 0.1e-6       # ±0.1 uA tolerance
-    imeas = AMEASURE(signal="ADDR", reference="GND", expected_value=expected_curr, error_spread=curr_error)
+    imeas = AMEASURE(signal="IODATA1", reference="GND", expected_value=expected_curr, error_spread=curr_error)
     print(f'Measured Current: {imeas * 1e6:.3f} uA [Target: {expected_curr * 1e6:.1f}uA ±{curr_error * 1e6:.1f}uA]')
     # Pass/Fail Criteria
     AMEASURE(signal="ADDR", reference="GND", expected_value=float('Inf')) # trun the Ammeter multimeter switch wtih "ADDR" pin
