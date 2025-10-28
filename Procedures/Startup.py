9c800d32a2e71afc515482c705a2340b5c3d2632
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-10-28 17:31:57 +0100

    Update test script Procedures/Startup.py: -# command = f"{sys.executable} -m pip uninstall dfttools -y" | -# os.system(command) | -# command = f"{sys.executable} -m pip install git+https://github.com/HarishKumarSedu/dfttools.git@main" | -# os.system(command) | +command = f"{sys.executable} -m pip uninstall dfttools -y"

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -2,10 +2,10 @@ from time import sleep
 import os 
 import sys
 # example code to install python pacakage
-# command = f"{sys.executable} -m pip uninstall dfttools -y"
-# os.system(command)
-# command = f"{sys.executable} -m pip install git+https://github.com/HarishKumarSedu/dfttools.git@main"
-# os.system(command)
+command = f"{sys.executable} -m pip uninstall dfttools -y"
+os.system(command)
+command = f"{sys.executable} -m pip install git+https://github.com/HarishKumarSedu/dfttools.git@main"
+os.system(command)
 
 from dfttools import *
 def startup():
