e419f4a419af7599ad1842874295fa0f0987af79
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-11-14 10:50:01 +0100

    Add/update multiple files commit

--- GPADC/GPADC_Vbat_2P8_Test.py
+++ GPADC/GPADC_Vbat_2P8_Test.py
@@ -31,25 +31,25 @@ def gpadc_vbat_2P8_test():
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'gpadc_refcomp_sel', 'length': 1, 'registers': [{'REG': '0xD6', 'POS': 0, 'RegisterName': 'SAR ADC settings ', 'RegisterLength': 8, 'Name': 'gpadc_refcomp_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'gpadc_en', 'length': 1, 'registers': [{'REG': '0xDF', 'POS': 0, 'RegisterName': 'SAR ADC settings ', 'RegisterLength': 8, 'Name': 'gpadc_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': 'NPPPNNNN', 'Default': '0x01', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
     pvdd_forced_voltage = VFORCE(signal="PVDD", reference="GND", value=2.8,error_spread=0)
-    # sleep(0.01)
-    # VB0 = VMEASURE(signal="PVDD", reference="GND", expected_value=2.8,error_spread=0)
-    # sleep(0.001)
-    # expected_value=round(((VB0*22/(22+79))*1024/(0.6*2)))
+    sleep(0.01)
+    VB0 = VMEASURE(signal="PVDD", reference="GND", expected_value=2.8,error_spread=0)
+    sleep(0.001)
+    expected_value=round(((VB0*22/(22+79))*1024/(0.6*2)))
 
-    # Vbat_min=expected_value-2
-    # Vbat_max=expected_value+2
+    Vbat_min=expected_value-2
+    Vbat_max=expected_value+2
 
-    # HCODE0=I2C_READ(device_address="0x38",field_info={'fieldname': 'vbat_meas', 'length': 10, 'registers': [{'REG': '0x23', 'POS': 0, 'RegisterName': 'VBAT measurement reg 1', 'RegisterLength': 8, 'Name': 'vbat_meas[9:8]', 'Mask': '0x3', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': '000000RR', 'Default': '0x00', 'User': '00YYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x24', 'POS': 0, 'RegisterName': 'VBAT measurement reg 2', 'RegisterLength': 8, 'Name': 'vbat_meas[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},expected_value=0x207)
-    # CODE0=int(HCODE0)
+    HCODE0=I2C_READ(device_address="0x38",field_info={'fieldname': 'vbat_meas', 'length': 10, 'registers': [{'REG': '0x23', 'POS': 0, 'RegisterName': 'VBAT measurement reg 1', 'RegisterLength': 8, 'Name': 'vbat_meas[9:8]', 'Mask': '0x3', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': '000000RR', 'Default': '0x00', 'User': '00YYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x24', 'POS': 0, 'RegisterName': 'VBAT measurement reg 2', 'RegisterLength': 8, 'Name': 'vbat_meas[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},expected_value=0x207)
+    CODE0=int(HCODE0)
 
-    # if Vbat_min <= CODE0 <= Vbat_max:
-    # print(f'............ {Test_Name} Passed ........')
-    #     # write the optimized code if the trim passed
+    if Vbat_min <= CODE0 <= Vbat_max:
+      print(f'............ {Test_Name} Passed ........')
+        # write the optimized code if the trim passed
 
 
-    # print(f"VBAT2P8 Code: {CODE0}")
-    # print(f"Vbat_min Code: {Vbat_min}")
-    # print(f"Vbat_max Code: {Vbat_max}")
+    print(f"VBAT2P8 Code: {CODE0}")
+    print(f"Vbat_min Code: {Vbat_min}")
+    print(f"Vbat_max Code: {Vbat_max}")
 
     # I2C_WRITE(device_address="0x38",field_info={'fieldname': 'gpadc_en', 'length': 1, 'registers': [{'REG': '0xDF', 'POS': 0, 'RegisterName': 'SAR ADC settings ', 'RegisterLength': 8, 'Name': 'gpadc_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': 'NPPPNNNN', 'Default': '0x01', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x0)
     # sleep(0.001)
