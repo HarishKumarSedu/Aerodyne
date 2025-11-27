92c0d53fe8667a857f984f92c4008d84aa553e93
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-11-27 10:29:12 +0100

    Add/update multiple files commit

--- Trimming/GPADC_Vbat_Gain_Off_Trim.py
+++ Trimming/GPADC_Vbat_Gain_Off_Trim.py
@@ -62,7 +62,7 @@ def gpadc_vbat_gain_off_trim():
 
    
     print(f"Optimal gain Code: {hex(otp_vbat_gain)}")
-    print(f"Optimal oofset Code: {hex(vbat_off)}")
+    print(f"Optimal offset Code: {hex(vbat_off)}")
 
     sleep(0.001)
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x0)
