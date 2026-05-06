aab26c78f33d1d1727d894c9a52f46c3d86828e4
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2026-05-06 10:27:48 +0200

    Add/update multiple files commit

--- Optim_Procedures/Initialization.py
+++ Optim_Procedures/Initialization.py
@@ -2,7 +2,7 @@ from dfttools import *
 
 def initialization():
     testname = 'Initialization'
-    print('*' * ((50 - len(testname)) // 2) + testname + '*' * ((50 - len(testname) + 1) // 2)) # do not bother too much about it will just align the test in between
+    print('*' * ((10 - len(testname)) // 2) + testname + '*' * ((10 - len(testname) + 1) // 2)) # do not bother too much about it will just align the test in between
     
 if __name__ == '__main__':
     initialization()
