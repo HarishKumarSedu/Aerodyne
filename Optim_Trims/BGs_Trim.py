4f22216bedaedd05ddd3e83632746a6b8912b114
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2026-05-07 16:40:49 +0200

    Add/update multiple files commit

--- Optim_Trims/BGs_Trim.py
+++ Optim_Trims/BGs_Trim.py
@@ -61,6 +61,7 @@ def bgs_trim():
             # WRITE DEFAULT
             I2C_WRITE(device_address="0x38",field_info=otp_field,write_value=0)
         ############### DATA LOGGING ########################
+        print(f'TEST-NAME  : {test_name} ')
         print(f'PRE -TRIM  : {pretrim_bg_measured_value:.6f} V')
         print(f"OTP CODE   : {otp_trim_code:#02X}")
         print(f'POST -TRIM : {posttrim_bg_measured_value:.6f} V')
@@ -76,6 +77,45 @@ def bgs_trim():
             'posttrim_bg_measured_value':posttrim_bg_measured_value,
             'error_%':min_error
         }
+    ############### SELF-REG LDO 1.2v ########################
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en_vos_buff', 'length': 1, 'registers': [{'REG': '0x10', 'POS': 7, 'RegisterName': 'FORCING_REG_2', 'RegisterLength': 8, 'Name': 'ref_test_en_vos_buff', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en_buff', 'length': 1, 'registers': [{'REG': '0x10', 'POS': 6, 'RegisterName': 'FORCING_REG_2', 'RegisterLength': 8, 'Name': 'ref_test_en_buff', 'Mask': '0x40', 'Length': 1, 'FieldMSB': 6, 'FieldLSB': 6, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
+    LDO_LSB = 15.14e-3
+    min_error = float('inf')
+    LDO_TARGET = 1.2
+    LDO_ERROR_PERCENTAGE = 1e-2
+    test_name='SELF_REG_LDO_1V2'
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'test_sel', 'length': 4, 'registers': [{'REG': '0x15', 'POS': 0, 'RegisterName': 'ANA_TESTMUX_SEL', 'RegisterLength': 8, 'Name': 'test_sel[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0xC) # PROGRAM THE TEST SEL CODE 
+    pretrim_ldo_measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=LDO_TARGET,error_spread=1*LDO_LSB)
+    ############### Trim code esitmation ################
+    delta_v     = LDO_TARGET - pretrim_ldo_measured_value          
+    raw_steps   = delta_v / LDO_LSB               # float steps needed
+    trim_code   = -round(raw_steps)              # standard rounding
+    print(pretrim_ldo_measured_value)
+    ############### Check for the Trim code range #######
+    if trim_code < min_code or trim_code > max_code:
+        raise RuntimeError(f"  [WARNING] Required steps ({trim_code}) out of range "
+                           f"[{min_code}, {max_code}] — clamping!")
+        trim_code = max(min_code, min(max_code, trim_code))
+    otp_trim_code = 2**num_bits + trim_code if trim_code <0 else trim_code
+    I2C_WRITE(device_address="0x38",field_info={'fieldname': 'otp_ds_ref_self_ref_trm_0v4', 'length': 4, 'registers': [{'REG': '0xCD', 'POS': 0, 'RegisterName': 'OTP FIELDS 29', 'RegisterLength': 8, 'Name': 'otp_ds_ref_self_ref_trm_0v4[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=otp_trim_code)
+    posttrim_ldo_measured_value = VMEASURE(signal="IODATA1", reference="GND", expected_value=LDO_TARGET,error_spread=LDO_LSB/2)
+    ############### LIMIT CHECK #######################
+    lower_limit = LDO_TARGET - LDO_TARGET*LDO_ERROR_PERCENTAGE
+    higher_limit = LDO_TARGET + LDO_TARGET*LDO_ERROR_PERCENTAGE
+    if lower_limit < posttrim_ldo_measured_value < higher_limit:
+        print(f'{test_name} PASSED !!!')
+    else:
+        # if the trimh failed program default zero
+        raise RuntimeError(f'{test_name} PASSED !!!')
+        # WRITE DEFAULT
+        I2C_WRITE(device_address="0x38",field_info={'fieldname': 'otp_ds_ref_self_ref_trm_0v4', 'length': 4, 'registers': [{'REG': '0xCD', 'POS': 0, 'RegisterName': 'OTP FIELDS 29', 'RegisterLength': 8, 'Name': 'otp_ds_ref_self_ref_trm_0v4[3:0]', 'Mask': '0xF', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0)
+    print(f'TEST-NAME  : {test_name} ')
+    print(f'PRE -TRIM  : {pretrim_ldo_measured_value:.6f} V')
+    print(f"OTP CODE   : {otp_trim_code:#02X}")
+    print(f'POST -TRIM : {posttrim_ldo_measured_value:.6f} V')
+    print(f'TARGET     : {target:.6f} V')
+    print(f"ERROR      : {min_error:.6f} %")
     ############### CLEANUP ########################
     VMEASURE(signal="IODATA1", reference="GND", expected_value=float('Inf'),comments='Remove Multimeter')
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'ref_test_en', 'length': 1, 'registers': [{'REG': '0x16', 'POS': 7, 'RegisterName': 'ANA_TESTMUX_EN1', 'RegisterLength': 8, 'Name': 'ref_test_en', 'Mask': '0x80', 'Length': 1, 'FieldMSB': 7, 'FieldLSB': 7, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=0x0)
