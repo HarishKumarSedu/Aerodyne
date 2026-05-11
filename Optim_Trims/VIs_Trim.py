3bbd933b28a80a9ccf7a4506ca0f08a9c141aba1
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2026-05-11 14:28:32 +0200

    Add/update multiple files commit

---
+++ Optim_Trims/VIs_Trim.py
@@ -0,0 +1,56 @@
+from dfttools import *
+from time import sleep
+from Procedures.utils import complement,dec_to_2complement
+from time import sleep
+
+def samples_average(fields=None, samples=8, sleep_time=5e-3):
+    if fields is None:
+        fields = []
+    no_fields = len(fields)
+    if no_fields == 0:
+        return []
+    # Initialize data with zeros for each field
+    data = [0] * no_fields
+    for _ in range(samples):
+        for field_no in range(no_fields):
+            field = fields[field_no].get('field', {})
+            expected_value = fields[field_no].get('expected_value', 0xffff)
+            raw_data = I2C_READ("0x38", field_info=field, expected_value=expected_value)
+            data[field_no] += complement(raw_data,field.get('length'),16)
+        sleep(sleep_time)
+    # Correct averaging: divide by samples (right-shift by log2(samples))
+    shift_amount = samples.bit_length() - 1  # For samples=8, shift=3
+    for field_no in range(no_fields):
+        data[field_no] = data[field_no] >> shift_amount
+    return data
+# def logger(*args,**kwargs):
+#     for k, v in kwargs.items():
+#         print(f'{k.upper()} : {v}')
+def VIs_Trim():
+    test_name = 'VIs_Trim'
+    print(f'...........{test_name}..........')
+    vLSB = 5.5/2**15
+    I2C_WRITE("0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0)
+    # I2C_WRITE("0x38", field_info={'fieldname': 'cld_dvr_force_sel', 'length': 8, 'registers': [{'REG': '0x9C', 'POS': 0, 'RegisterName': 'CLD analog setting reg 6', 'RegisterLength': 8, 'Name': 'cld_dvr_force_sel[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0)
+    I2C_WRITE("0x38", field_info={'fieldname': 'cld_drv_force', 'length': 1, 'registers': [{'REG': '0x9B', 'POS': 4, 'RegisterName': 'CLD analog setting reg 5', 'RegisterLength': 8, 'Name': 'cld_drv_force', 'Mask': '0x10', 'Length': 1, 'FieldMSB': 4, 'FieldLSB': 4, 'Attribute': 'NNNNNNNN', 'Default': '0x81', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=1)
+    I2C_WRITE("0x38", field_info={'fieldname': 'i2c_page_sel_1', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel_1', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=1)
+    # turn the Vmid buffer Vmid = Vddp/2
+    I2C_WRITE("0x38", field_info={'fieldname': 'cld_vmid_buf_en_m', 'length': 1, 'registers': [{'REG': '0x1B', 'POS': 6, 'RegisterName': 'Force registers 4', 'RegisterLength': 8, 'Name': 'cld_vmid_buf_en_m', 'Mask': '0x40', 'Length': 1, 'FieldMSB': 6, 'FieldLSB': 6, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=1)
+    I2C_WRITE("0x38", field_info={'fieldname': 'cld_vmid_buf_en_force', 'length': 1, 'registers': [{'REG': '0x1A', 'POS': 6, 'RegisterName': 'Force registers 3', 'RegisterLength': 8, 'Name': 'cld_vmid_buf_en_force', 'Mask': '0x40', 'Length': 1, 'FieldMSB': 6, 'FieldLSB': 6, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=1)
+    I2C_WRITE("0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0)
+    # put the output stage to the common mode at the VDDP/2
+    I2C_WRITE("0x38", field_info={'fieldname': 'cld_dvr_force_sel', 'length': 8, 'registers': [{'REG': '0x9C', 'POS': 0, 'RegisterName': 'CLD analog setting reg 6', 'RegisterLength': 8, 'Name': 'cld_dvr_force_sel[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=0x66)
+    vsns_offset_otp_length = {'fieldname': 'otp_vsense_offset', 'length': 6, 'registers': [{'REG': '0xB8', 'POS': 0, 'RegisterName': 'OTP FIELDS 8', 'RegisterLength': 8, 'Name': 'otp_vsense_offset[5:0]', 'Mask': '0x3F', 'Length': 6, 'FieldMSB': 5, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}.get( 'length',6)
+    [vsns_offset_pretrim_code] = samples_average([{'field':{'fieldname': 'v_sense', 'length': 16, 'registers': [{'REG': '0x69', 'POS': 0, 'RegisterName': 'V SENSE readback reg 1', 'RegisterLength': 8, 'Name': 'v_sense[15:8]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 15, 'FieldLSB': 8, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x6A', 'POS': 0, 'RegisterName': 'V SENSE readback reg 2', 'RegisterLength': 8, 'Name': 'v_sense[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},'expected_value':0xfffc}])
+    vsns_pretrim_offset_value = vsns_offset_pretrim_code*vLSB
+    vns_offset_otp_code = dec_to_2complement(vsns_offset_pretrim_code>>3,vsns_offset_otp_length)
+    I2C_WRITE("0x38", field_info={'fieldname': 'i2c_page_sel', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '000000YY', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]}, write_value=1)
+    I2C_WRITE("0x38", field_info={'fieldname': 'otp_vsense_offset', 'length': 6, 'registers': [{'REG': '0xB8', 'POS': 0, 'RegisterName': 'OTP FIELDS 8', 'RegisterLength': 8, 'Name': 'otp_vsense_offset[5:0]', 'Mask': '0x3F', 'Length': 6, 'FieldMSB': 5, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}, write_value=vns_offset_otp_code)
+    [vsns_offset_posttrim_code] = samples_average([{'field':{'fieldname': 'v_sense', 'length': 16, 'registers': [{'REG': '0x69', 'POS': 0, 'RegisterName': 'V SENSE readback reg 1', 'RegisterLength': 8, 'Name': 'v_sense[15:8]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 15, 'FieldLSB': 8, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x6A', 'POS': 0, 'RegisterName': 'V SENSE readback reg 2', 'RegisterLength': 8, 'Name': 'v_sense[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': 'YYYYYYYY', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},'expected_value':0xffff}])
+    vsns_posttrim_offset_value = vsns_offset_posttrim_code*vLSB
+    print(f'VSNS OFFSET:~')
+    print(f'PRE-TRIM CODE   : {vsns_offset_pretrim_code:04x}')
+    print(f'PRE-TRIM OFFSET : {vsns_pretrim_offset_value} V')
+    print(f'OTP CODE        : {vns_offset_otp_code:04x}')
+    print(f'POST-TRIM OFFSET: {vsns_posttrim_offset_value} V')
+    print(f'PRE-TRIM CODE : {vsns_offset_pretrim_code:04x}')
