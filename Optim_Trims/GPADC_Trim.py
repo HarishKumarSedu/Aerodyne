d28cd49893d72957179419066d14497755e6591f
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2026-05-21 10:37:07 +0200

    Add/update multiple files commit

--- Optim_Trims/GPADC_Trim.py
+++ Optim_Trims/GPADC_Trim.py
@@ -21,6 +21,22 @@ def samples_average(fields=None, samples=8, sleep_time=5e-3):
     for field_no in range(no_fields):
         data[field_no] = data[field_no] >> shift_amount
     return data
+def vbat_gpadc_gain_offset_cal(data:{}):
+    VBAT_LSB=5.3763441E-03
+    vbat_gain_otp_length = {'fieldname': 'otp_sar_gain_vbat', 'length': 8, 'registers': [{'REG': '0xC2', 'POS': 0, 'RegisterName': 'OTP FIELDS 18 - TRACEABILITY 6', 'RegisterLength': 8, 'Name': 'otp_sar_gain_vbat[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}.get( 'length',8)
+    vbat_offset_otp_length = {'fieldname': 'otp_sar_offs', 'length': 10, 'registers': [{'REG': '0xBF', 'POS': 4, 'RegisterName': 'OTP FIELDS 15 - TRACEABILITY 3', 'RegisterLength': 8, 'Name': 'otp_sar_offs[9:8]', 'Mask': '0x30', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}, {'REG': '0xC1', 'POS': 0, 'RegisterName': 'OTP FIELDS 17 - TRACEABILITY 5', 'RegisterLength': 8, 'Name': 'otp_sar_offs[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}.get( 'length',8)
+    VB1_4P8v_pretrim_V = data.get('VB1_4P8v_pretrim_V')
+    VB0_2P8v_pretrim_V = data.get('VB0_2P8v_pretrim_V')
+    VB1_4P8v_pretrim_code = data.get('VB1_4P8v_pretrim_code')
+    VB0_2P8v_pretrim_code = data.get('VB0_2P8v_pretrim_code')
+    vbat_gain=(VB1_4P8v_pretrim_V-VB0_2P8v_pretrim_V)/(VB1_4P8v_pretrim_code-VB0_2P8v_pretrim_code)
+    vbat_off=round((VB0_2P8v_pretrim_V/vbat_gain)-VB0_2P8v_pretrim_code)
+    vbat_gain_correction_scaled=round((vbat_gain/VBAT_LSB-1)*1024)
+    # complement the value 
+    vbat_gain_otp_code = dec_to_2complement(vbat_gain_correction_scaled,vbat_gain_otp_length,False)
+    vbat_offset_otp_code = dec_to_2complement(vbat_off,vbat_offset_otp_length,False)
+    return {'vbat_gain':vbat_gain,'vbat_off':vbat_off,'vbat_gain_otp_code':vbat_gain_otp_code,'vbat_offset_otp_code':vbat_offset_otp_code}
+
 def gpadc_vbat_gain_off_trim():
     Test_Name = 'GPADC_TRIMMING'
     print(f'............ {Test_Name} ........')
@@ -34,22 +50,18 @@ def gpadc_vbat_gain_off_trim():
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'gpadc_en', 'length': 1, 'registers': [{'REG': '0xDF', 'POS': 0, 'RegisterName': 'SAR ADC settings ', 'RegisterLength': 8, 'Name': 'gpadc_en', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': 'NPPPNNNN', 'Default': '0x03', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'gpadc_refcomp_sel', 'length': 1, 'registers': [{'REG': '0xD6', 'POS': 0, 'RegisterName': 'SAR ADC settings ', 'RegisterLength': 8, 'Name': 'gpadc_refcomp_sel', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x01', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG0'}]},write_value=0x1)
     ###################### GPAFC VBAT GAIN AND OFFSET PRETIMMING ###################
-    VBAT_LSB=5.3763441E-03
-    vbat_gain_otp_length = {'fieldname': 'otp_sar_gain_vbat', 'length': 8, 'registers': [{'REG': '0xC2', 'POS': 0, 'RegisterName': 'OTP FIELDS 18 - TRACEABILITY 6', 'RegisterLength': 8, 'Name': 'otp_sar_gain_vbat[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}.get( 'length',8)
-    vbat_offset_otp_length = {'fieldname': 'otp_sar_offs', 'length': 10, 'registers': [{'REG': '0xBF', 'POS': 4, 'RegisterName': 'OTP FIELDS 15 - TRACEABILITY 3', 'RegisterLength': 8, 'Name': 'otp_sar_offs[9:8]', 'Mask': '0x30', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}, {'REG': '0xC1', 'POS': 0, 'RegisterName': 'OTP FIELDS 17 - TRACEABILITY 5', 'RegisterLength': 8, 'Name': 'otp_sar_offs[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG1'}]}.get( 'length',8)
+    
     # SET VBAT@2.8V and read the 8 sample averaged code
     VB0_2P8v_pretrim_V = VFORCE(signal="PVDD", reference="GND", value=2.8,error_spread=0)
     [VB0_2P8v_pretrim_code]=samples_average([{'field':{'fieldname': 'vbat_meas', 'length': 10, 'registers': [{'REG': '0x23', 'POS': 0, 'RegisterName': 'VBAT measurement reg 1', 'RegisterLength': 8, 'Name': 'vbat_meas[9:8]', 'Mask': '0x3', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': '000000RR', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x24', 'POS': 0, 'RegisterName': 'VBAT measurement reg 2', 'RegisterLength': 8, 'Name': 'vbat_meas[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},'expected_value':517}])
     # SET VBAT@4.8V and read the 8 sample averaged code
     VB1_4P8v_pretrim_V = VFORCE(signal="PVDD", reference="GND", value=4.8,error_spread=0)
     [VB1_4P8v_pretrim_code]=samples_average([{'field':{'fieldname': 'vbat_meas', 'length': 10, 'registers': [{'REG': '0x23', 'POS': 0, 'RegisterName': 'VBAT measurement reg 1', 'RegisterLength': 8, 'Name': 'vbat_meas[9:8]', 'Mask': '0x3', 'Length': 2, 'FieldMSB': 9, 'FieldLSB': 8, 'Attribute': '000000RR', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}, {'REG': '0x24', 'POS': 0, 'RegisterName': 'VBAT measurement reg 2', 'RegisterLength': 8, 'Name': 'vbat_meas[7:0]', 'Mask': '0xFF', 'Length': 8, 'FieldMSB': 7, 'FieldLSB': 0, 'Attribute': 'RRRRRRRR', 'Default': '0x00', 'User': '00000000', 'Clocking': 'REF', 'Reset': 'C', 'PageName': 'PAG0'}]},'expected_value':897}])
-
-    vbat_gain=(VB1_4P8v_pretrim_V-VB0_2P8v_pretrim_V)/(VB1_4P8v_pretrim_code-VB0_2P8v_pretrim_code)
-    vbat_off=round((VB0_2P8v_pretrim_V/vbat_gain)-VB0_2P8v_pretrim_code)
-    vbat_gain_correction_scaled=round((vbat_gain/VBAT_LSB-1)*1024)
-    # complement the value 
-    vbat_gain_otp_code = dec_to_2complement(vbat_gain_correction_scaled,vbat_gain_otp_length,False)
-    vbat_offset_otp_code = dec_to_2complement(vbat_off,vbat_offset_otp_length,False)
+    pretrim_results = vbat_gpadc_gain_offset_cal({'VB0_2P8v_pretrim_V':VB0_2P8v_pretrim_V,'VB0_2P8v_pretrim_code':VB0_2P8v_pretrim_code,'VB1_4P8v_pretrim_V':VB1_4P8v_pretrim_V,'VB1_4P8v_pretrim_code':VB1_4P8v_pretrim_code})
+    vbat_offset_otp_code = pretrim_results['vbat_offset_otp_code']
+    vbat_gain_otp_code = pretrim_results['vbat_gain_otp_code']
+    vbat_pretrim_gain = pretrim_results['vbat_gain']
+    vbat_pretrim_offset = pretrim_results['vbat_off']
     print(f'............ {Test_Name} Passed ........')
         # write the optimized code if the trim passed
     I2C_WRITE(device_address="0x38",field_info={'fieldname': 'i2c_page_sel_1', 'length': 1, 'registers': [{'REG': '0xFE', 'POS': 0, 'RegisterName': 'Page selection', 'RegisterLength': 8, 'Name': 'i2c_page_sel_1', 'Mask': '0x1', 'Length': 1, 'FieldMSB': 0, 'FieldLSB': 0, 'Attribute': '0000000N', 'Default': '0x00', 'User': '00000000', 'Clocking': 'SMB', 'Reset': 'C', 'PageName': 'PAG1'}]},write_value=1)
@@ -59,7 +71,7 @@ def gpadc_vbat_gain_off_trim():
     print(f'GPADC-VBAT GAIN OFFSET :~')
     print(f'VB0 (@ 2.8V) : {VB0_2P8v_pretrim_V:.6F} V , CODE0 : [ {VB0_2P8v_pretrim_code :#04X} ]')
     print(f'VB1 (@ 2.8V) : {VB1_4P8v_pretrim_V:.6F} V , CODE1 : [ {VB1_4P8v_pretrim_code :#04X} ]')
-    print(f'VBAT PRE-GAIN: {vbat_gain:.6F}')
+    print(f'VBAT PRE :> GAIN : {vbat_pretrim_gain:.6F}, OFFSET : {vbat_pretrim_offset:.6F}')
     print(f'VBAT-GAIN OTP CODE : [ {vbat_gain_otp_code:#04X} ], VBAT-OFFSET OTP CODE : [ {vbat_offset_otp_code:#04X} ]')
 
    
